3dbe522db092446b9085a0a9912677f4088db7d6
Eren Halici <eren@halici.com.tr>
2012-08-16 18:10:32 +0300

    removed public assets

--- public/assets/processFiles-a4eca39e2c4ee455b6d7c46521a387d9.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# grep -rl 'console' animations/ | xargs sed -i .old 's|console|//console|g'
-# rm animations/*.old
-# grep -rl '//' animations/ | xargs sed -i .old 's|////|//|g'
-# rm animations/*.old
