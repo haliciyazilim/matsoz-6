0c249dc20ff89e8053df3f10c4ab4214230ebc0c
Alperen Kavun <alperen@halici.com.tr>
2012-08-17 10:29:31 +0300

    bolunebilme interaction splitting are done

--- app/assets/javascripts/animations/bolunebilme/bolunebilme.styles.js
+++ app/assets/javascripts/animations/bolunebilme/bolunebilme.styles.js
@@ -1,5 +1,21 @@
 function __Styles(){
-	/*
-	* write your styles here without using 'var' definer
-	*/
+    wholeTableFillColor = "white";
+    wholeTableStrokeColor = "#a8dbe3";
+
+    wholeTablePrimeFillColor = "#9ee9a5";
+    wholeTablePrimeStrokeColor = "#4f9c4f";
+
+    counterColor = "#a8dbe3";
+    counterQuestionColor = "#41818a"
+
+    animationRectFillColor = "e99e9e";
+    animationRectStrokeColor = "9c4f4f"
+
+    animationRectFillColor2 = "f2c885";
+    animationRectStrokeColor2 = "9b763d";
+
+    textColor = "#006e7d";
+    textColor2 = "#FF0000"; // not divisible color
+    textColor3 = "#000000"; // original color
+    textColor4 = "#006e7d" // divisible color
 }
