95a1d1c9fc4f0bfb929a66ba0868d64b86f4e4e1
Alperen Kavun <alperen@halici.com.tr>
2012-08-22 17:46:38 +0300

    Kume Class are added

--- app/assets/javascripts/animations/kume/kume.common.js
+++ app/assets/javascripts/animations/kume/kume.common.js
@@ -1,16 +1,367 @@
-var Set = {
-    ELEMENTS : 0,
-    SMALLER_THAN : 1,
-    SMALLER_THAN_ODD : 2,
-    SMALLER_THAN_EVEN : 3,
-    SMALER_THAN_PRIME : 4,
-    SMALLER_THAN_GREATER_THAN : 5,
-    SMALLER_THAN_GREATER_THAN_ODD : 6,
-    SMALLER_THAN_GREATER_THAN_EVEN : 7,
-    SMALLER_THAN_GREATER_THAN_PRIME : 8,
-    FACTORS : 9,
-    MULTIPLİES : 10,
-    init:function(){
+var Set = Class.extend({
+    init:function(opt){
+        this.elements = [];
+        switch(opt.type){
+            case Set.ELEMENTS:
+                this.definition = ""+opt.elements.length+" elemanlı küme";
+                this.elements = opt.elements;
+                this.elements.sort(function(a,b){return a-b})
+                this.type = Set.ELEMENTS;
+                break;
+            case Set.SMALLER_THAN:
+                this.definition = ""+this.getValueStr(opt.value)+" küçük doğal sayılar";
+                for(var i = 0; i < opt.value; i++){
+                    this.elements.push(i);
+                }
+                this.type = Set.SMALLER_THAN;
+                break;
+
+            case Set.SMALLER_THAN_ODD:
+                this.definition = ""+this.getValueStr(opt.value)+" küçük tek doğal sayılar";
+                for(var i = 0; i < opt.value; i++){
+                    if(i % 2 == 1){
+                        this.elements.push(i);
+                    }
+                }
+                this.type = Set.SMALLER_THAN_ODD;
+                break;
+            case Set.SMALLER_THAN_EVEN:
+                this.definition = ""+this.getValueStr(opt.value)+" küçük çift doğal sayılar";
+                for(var i = 0; i < opt.value; i++){
+                    if(i % 2 == 0){
+                        this.elements.push(i);
+                    }
+                }
+                this.type = Set.SMALLER_THAN_EVEN;
+                break;
+            case Set.SMALLER_THAN_PRIME:
+                this.definition = ""+this.getValueStr(opt.value)+" küçük asal sayılar";
+                for(var i = 0; i < opt.value; i++){
+                    if(Util.isPrimeNumber(i)){
+                        this.elements.push(i);
+                    }
+                }
+                this.type = Set.SMALLER_THAN_PRIME;
+                break;
+            case Set.SMALLER_THAN_GREATER_THAN:
+                this.definition = ""+this.getValueStr(opt.value1)+" büyük "+this.getValueStr(opt.value2)+" küçük doğal sayılar";
+                for(var i = opt.value1+1; i < opt.value2; i++){
+                    this.elements.push(i);
+                }
+                this.type = Set.SMALLER_THAN_GREATER_THAN;
+                break;
+            case Set.SMALLER_THAN_GREATER_THAN_ODD:
+                this.definition = ""+this.getValueStr(opt.value1)+" büyük "+this.getValueStr(opt.value2)+" küçük tek doğal sayılar";
+                for(var i = opt.value1+1; i < opt.value2; i++){
+                    if(i % 2 == 1){
+                        this.elements.push(i);
+                    }
+                }
+                this.type = Set.SMALLER_THAN_GREATER_THAN_ODD;
+                break;
+            case Set.SMALLER_THAN_GREATER_THAN_EVEN:
+                this.definition = ""+this.getValueStr(opt.value1)+" büyük "+this.getValueStr(opt.value2)+" küçük çift doğal sayılar";
+                for(var i = opt.value1+1; i < opt.value2; i++){
+                    if(i % 2 == 0){
+                        this.elements.push(i);
+                    }
+                }
+                this.type = Set.SMALLER_THAN_GREATER_THAN_EVEN;
+                break;
+            case Set.SMALLER_THAN_GREATER_THAN_PRIME:
+                this.definition = ""+this.getValueStr(opt.value1)+" büyük "+this.getValueStr(opt.value2)+" küçük asal sayılar";
+                for(var i = opt.value1+1; i < opt.value2; i++){
+                    if(Util.isPrimeNumber(i)){
+                        this.elements.push(i);
+                    }
+                }
+                this.type = Set.SMALLER_THAN_GREATER_THAN_PRIME;
+                break;
+            case Set.FACTORS:
+                this.definition = ""+this.getValueStr(opt.value, 1)+" çarpanları";
+                a = [];
+                a = Util.getFactors(opt.value);
+                for(var i = 0; i < a.length; i++){
+                    this.elements.push(a[i]);
+                }
+                this.type = Set.FACTORS;
+                break;
+            case Set.MULTIPLIES:
+                this.definition = ""+this.getValueStr(opt.value1,1)+" "+this.getValueStr(opt.value2)+" küçük katları";
+                for(var i = 1; i < opt.value2/opt.value1; i++){
+                    this.elements.push(opt.value1*i);
+                }
+                this.type = Set.MULTIPLIES;
+                break;
+        }
+    },
+
+    isEqualSet:function(otherSet){
+        if(this.elements.length != otherSet.elements.length){
+            return false;
+        }
+        else{
+            var a = [];
+            var correctN = 0;
+            for(var i = 0; i < this.elements.length; i++){
+                a[i] = this.elements[i];
+            }
+            for(var i = 0; i < this.elements.length; i++){
+                for(var j = 0; j < otherSet.elements.length; j++){
+                    if(a[i] == otherSet.elements[j]){
+                        a[i] = "axxwt";
+                    }
+                }
+            }
+
+            for(var i = 0; i < a.length; i++){
+                if(a[i] == "axxwt"){
+                    correctN += 1;
+                }
+            }
+
+            if(correctN == this.elements.length){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    },
+
+    isSubSet:function(otherSet){
+        if(this.elements.length > otherSet.elements.length){
+            return false;
+        }
+        else{
+            var a = [];
+            var correctN = 0;
+            for(var i = 0; i < this.elements.length; i++){
+                a[i] = this.elements[i];
+            }
+            for(var i = 0; i < this.elements.length; i++){
+                for(var j = 0; j < otherSet.elements.length; j++){
+                    if(a[i] == otherSet.elements[j]){
+                        a[i] = "axxwt";
+                    }
+                }
+            }
+
+            for(var i = 0; i < a.length; i++){
+                if(a[i] == "axxwt"){
+                    correctN += 1;
+                }
+            }
+
+            if(correctN == this.elements.length){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    },
+
+    isDiscreteSet:function(otherSet){
+        var a = [];
+        var correctN = 0;
+        for(var i = 0; i < this.elements.length; i++){
+            a[i] = this.elements[i];
+        }
+        for(var i = 0; i < this.elements.length; i++){
+            for(var j = 0; j < otherSet.elements.length; j++){
+                if(a[i] == otherSet.elements[j]){
+                    a[i] = "axxwt";
+                }
+            }
+        }
+
+        for(var i = 0; i < a.length; i++){
+            if(a[i] == "axxwt"){
+                correctN += 1;
+            }
+        }
+
+        if(correctN == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    },
+
+    isConcurrentSet:function(otherSet){
+        var a = [];
+        var correctN = 0;
+        for(var i = 0; i < this.elements.length; i++){
+            a[i] = this.elements[i];
+        }
+        for(var i = 0; i < this.elements.length; i++){
+            for(var j = 0; j < otherSet.elements.length; j++){
+                if(a[i] == otherSet.elements[j]){
+                    a[i] = "axxwt";
+                }
+            }
+        }
+
+        for(var i = 0; i < a.length; i++){
+            if(a[i] == "axxwt"){
+                correctN += 1;
+            }
+        }
+
+        if(correctN != 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    },
+
+    getIntersection:function(otherSet){
+        var intersect = [];
+        for(var i = 0; i < this.elements.length; i++){
+            for(var j = 0; j < otherSet.elements.length; j++){
+                if(this.elements[i] == otherSet.elements[j]){
+                    intersect.push(this.elements[i]);
+                }
+            }
+        }
+        intersect.sort(function(a,b){return a-b});
+        var c = new Set({type:Set.ELEMENTS, elements:intersect});
+        var cStr = "";
+        if(otherSet.type == Set.ELEMENTS){
+            cStr = "'nin";
+        }
+        else if(otherSet.type == Set.FACTORS || otherSet.type == Set.MULTIPLIES){
+            cStr = "'nın";
+        }
+        else{
+            cStr = "'ın"
+        }
+        c.definition = ""+this.definition+" ile "+otherSet.definition+""+cStr+" kesişimi";
+
+        return c;
+    },
+
+    getUnion:function(otherSet){
+        var union = [];
+
+        for(var i = 0; i < this.elements.length; i++){
+            union.push(this.elements[i]);
+        }
+
+        for(var j = 0; j < otherSet.elements.length; j++){
+            if(union.indexOf(otherSet.elements[j]) == -1){
+                union.push(otherSet.elements[j]);
+            }
+        }
+
+        union.sort(function(a,b){return a-b});
+        var c = new Set({type:Set.ELEMENTS, elements:union});
+        var cStr = "";
+        if(otherSet.type == Set.ELEMENTS){
+            cStr = "'nin";
+        }
+        else if(otherSet.type == Set.FACTORS || otherSet.type == Set.MULTIPLIES){
+            cStr = "'nın";
+        }
+        else{
+            cStr = "'ın"
+        }
+        c.definition = ""+this.definition+" ile "+otherSet.definition+""+cStr+" birleşimi";
+        return c;
+    },
+
+    getSubSets:function(){
 
     },
-};
+
+    getValueStr:function(value,opt){
+        if(opt == undefined){
+            opt = 0;
+        }
+        if(opt == 0){
+            var a = "";
+            if(value % 90 == 0){
+                a += value+"'dan";
+            }
+            else if(value % 80 == 0 || value % 50 == 0 ){
+                a += value+"'den";
+            }
+            else if(value % 70 == 0){
+                a += value+"'ten";
+            }
+            else if(value % 60 == 0 || value % 40 == 0){
+                a += value+"'tan";
+            }
+            else if(value % 30 == 0){
+                a += value+"'dan";
+            }
+            else if(value % 20 == 0){
+                a += value+"'den";
+            }
+            else if(value % 10 == 0){
+                a += value+"'dan";
+            }
+            else if(value % 10 == 1 || value % 10 == 2 || value % 10 == 7 || value % 10 == 8){
+                a += value+"'den";
+            }
+            else if(value % 10 == 3 || value % 10 == 4 || value % 10 == 5){
+                a += value+"'ten";
+            }
+            else if(value % 10 == 6 || value % 10 == 9){
+                a += value+"'dan";
+            }
+        }
+        else if(opt == 1){
+            var a = "";
+            if(value % 90 == 0 || value % 60 == 0){
+                a += value+"'ın";
+            }
+            else if(value % 80 == 0 || value % 70 == 0){
+                a += value+"'in";
+            }
+            else if(value % 50 == 0){
+                a += value+"'nin";
+            }
+            else if(value % 40 == 0){
+                a += value+"'ın";
+            }
+            else if(value % 30 == 0){
+                a += value+"'un";
+            }
+            else if(value % 20 == 0){
+                a += value+"'nin";
+            }
+            else if(value % 10 == 0 || value % 10 == 9){
+                a += value+"'un";
+            }
+            else if(value % 10 == 8 || value % 10 == 1 || value % 10 == 5){
+                a += value+"'in";
+            }
+            else if(value % 10 == 7 || value % 10 == 2){
+                a += value+"'nin";
+            }
+            else if(value % 10 == 6){
+                a += value+"'nın";
+            }
+            else if(value % 10 == 4 || value % 10 == 3){
+                a += value+"'ün";
+            }
+        }
+
+        return a;
+    }
+});
+Set.ELEMENTS = 0;
+Set.SMALLER_THAN = 1;
+Set.SMALLER_THAN_ODD = 2;
+Set.SMALLER_THAN_EVEN = 3;
+Set.SMALLER_THAN_PRIME = 4;
+Set.SMALLER_THAN_GREATER_THAN = 5;
+Set.SMALLER_THAN_GREATER_THAN_ODD = 6;
+Set.SMALLER_THAN_GREATER_THAN_EVEN = 7;
+Set.SMALLER_THAN_GREATER_THAN_PRIME = 8;
+Set.FACTORS = 9;
+Set.MULTIPLIES = 10;
