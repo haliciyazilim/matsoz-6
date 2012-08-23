2523260b2a0c95acd6b32eb0990eb4c59779c5ac
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-08-23 15:42:44 +0300

    kume.common.js is included

--- app/assets/javascripts/animations/kumelerin_birbiriyle_iliskisi/kumelerin_birbiriyle_iliskisi.includes.js
+++ app/assets/javascripts/animations/kumelerin_birbiriyle_iliskisi/kumelerin_birbiriyle_iliskisi.includes.js
@@ -1,3 +1,4 @@
+//= require ../kume/kume.common.js
 //= require ./kumelerin_birbiriyle_iliskisi.styles.js
 //= require ./kumelerin_birbiriyle_iliskisi.common.js
 //= require ./kumelerin_birbiriyle_iliskisi.animation.js
