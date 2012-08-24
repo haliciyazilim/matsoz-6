7b4f35bb33a82f6c217042309e96b5c010f372aa
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-08-24 13:58:53 +0300

    Entry kumelerle_fark_islemi is created

--- app/assets/javascripts/animations/kumelerin_fark_islemi/kumelerin_fark_islemi.includes.js
+++
@@ -1,4 +0,0 @@
-//= require ./kumelerin_fark_islemi.styles.js
-//= require ./kumelerin_fark_islemi.common.js
-//= require ./kumelerin_fark_islemi.animation.js
-//= require ./kumelerin_fark_islemi.interaction.js 
---
+++ app/assets/javascripts/animations/kumelerle_fark_islemi/kumelerle_fark_islemi.includes.js
@@ -0,0 +1,4 @@
+//= require ./kumelerle_fark_islemi.styles.js
+//= require ./kumelerle_fark_islemi.common.js
+//= require ./kumelerle_fark_islemi.animation.js
+//= require ./kumelerle_fark_islemi.interaction.js 
