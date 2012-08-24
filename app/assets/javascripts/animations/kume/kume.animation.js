e4fbd5d01c9fd590e31d525af2a13f3fe84f42fa
Alperen Kavun <alperen@halici.com.tr>
2012-08-24 11:15:50 +0300

    Main.animationFinished() are added

--- app/assets/javascripts/animations/kume/kume.animation.js
+++ app/assets/javascripts/animations/kume/kume.animation.js
@@ -1,7 +1,14 @@
 var Animation = {
     images:[],
 	init:function(container){
-			Animation.container = container;
-		
-		}
+        Animation.container = container;
+
+//        var topLeftPoint = new Point(120,50);
+//        var setLetter = "T";
+//        Animation.venn = Set.randomGenerator(5);
+//        Animation.venn.drawVennDiagram(Animation.container, topLeftPoint, setLetter);
+
+        Main.animationFinished();
+
+    }
 }
