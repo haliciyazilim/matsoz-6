132540b9568047cd08183f3ad9e927bab76392d1
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-08-24 14:02:40 +0300

    kume.common.js is included

--- app/assets/javascripts/animations/kumelerle_fark_islemi/kumelerle_fark_islemi.includes.js
+++ app/assets/javascripts/animations/kumelerle_fark_islemi/kumelerle_fark_islemi.includes.js
@@ -1,4 +1,5 @@
 //= require ./kumelerle_fark_islemi.styles.js
 //= require ./kumelerle_fark_islemi.common.js
 //= require ./kumelerle_fark_islemi.animation.js
-//= require ./kumelerle_fark_islemi.interaction.js 
+//= require ./kumelerle_fark_islemi.interaction.js
+//= require ../kume/kume.common.js
