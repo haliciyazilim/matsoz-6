292abf97d42365d9cfc8733cdd1c87458f5cd22e
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-08-28 15:51:17 +0300

    test

--- app/assets/javascripts/animations/kumelerin_birbiriyle_iliskisi/kumelerin_birbiriyle_iliskisi.animation.js
+++ app/assets/javascripts/animations/kumelerin_birbiriyle_iliskisi/kumelerin_birbiriyle_iliskisi.animation.js
@@ -1,7 +1,24 @@
 var Animation = {
     images:[],
 	init:function(container){
-			Animation.container = container;
-		
-		}
+        Animation.container = container;
+        Animation.referencePoint = new Point(300,50);
+        Animation.showEqualSets();
+    },
+    showEqualSets:function(){
+        var set1 = new Set({type:Set.ELEMENTS,elements:[1,2,3,4,5]});
+        set1.drawVennDiagram(Animation.container,Animation.referencePoint.add(-0,0),"A");
+        var set2 = new Set({type:Set.ELEMENTS,elements:[1,2,3,4,5]});
+        set2.drawVennDiagram(Animation.container,Animation.referencePoint.add(0,0),"A");
+    },
+    showDisjointSets:function(){
+
+    },
+    showSubsets:function(){
+
+    },
+    showIntersectingSets:function(){
+
+    }
+
 }
