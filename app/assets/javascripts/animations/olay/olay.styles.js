583246e381e05e9db1eb2e8f5ba951a7b74ecee0
Alperen Kavun <alperen@halici.com.tr>
2012-08-29 17:55:39 +0300

    styles for animation and interaction part are added completely
    Signed-off-by: Alperen Kavun <alperen@halici.com.tr>

--- app/assets/javascripts/animations/olay/olay.styles.js
+++ app/assets/javascripts/animations/olay/olay.styles.js
@@ -1,5 +1,70 @@
 function __Styles(){
-	/*
-	* write your styles here without using 'var' definer
-	*/
+	ballStyle = {
+        strokeColor : "#9b763d",
+        fillColor : "#f2c885"
+    };
+    ballTextColor = "#8b5400";
+
+    animDivStyle = {
+        position:'absolute',
+        top:'20px',
+        left:'320px',
+        height:'160px',
+        width:'450px',
+        //border:'1px solid',
+        fontSize:'16px'
+    };
+    firstDivStyle = {
+        position:'absolute',
+        top:'0px',
+        left:'0px',
+        opacity:0
+    };
+    secondDivStyle = {
+        position:'absolute',
+        top:'36px',
+        left:'191px',
+        opacity:0
+    };
+    thirdDivStyle = {
+        position:'absolute',
+        top:'82px',
+        left:'0px',
+    };
+    fourthDivStyle = {
+        position:'absolute',
+        top:'140px',
+        left:'70px',
+        opacity:0
+    };
+
+    optionsStyle = {
+        position:'absolute',
+        fontSize:'16px',
+        fontWeight:'bold',
+        color:'#000',
+        cursor:'pointer',
+        width:'300px'
+    }
+    optionsImageContainer = {
+        position:'relative',
+        width:'32px',
+        height:'32px',
+        float:'left',
+        top:'-8px',
+        marginRight:'10px',
+        backgroundImage:'url(/assets/radio_buttons.png)',
+        backgroundPosition:'0px 0px'
+    };
+    selectedOptionStyle = {
+        color:'#235394'
+    };
+
+    trueOptionStyle = {
+        color:'#309423'
+    };
+
+    falseOptionStyle = {
+        color:'#942323'
+    };
 }
