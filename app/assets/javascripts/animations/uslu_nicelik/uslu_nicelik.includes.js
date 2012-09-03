9e9db6af8f82dbe13e87cd56026a9827bdde188b
Alperen Kavun <alperen@halici.com.tr>
2012-09-03 17:34:37 +0300

    new interaction files are added
    Signed-off-by: Alperen Kavun <alperen@halici.com.tr>

---
+++ app/assets/javascripts/animations/uslu_nicelik/uslu_nicelik.includes.js
@@ -0,0 +1,4 @@
+//= require ./uslu_nicelik.styles.js
+//= require ./uslu_nicelik.common.js
+//= require ./uslu_nicelik.animation.js
+//= require ./uslu_nicelik.interaction.js 
---
+++ app/assets/javascripts/animations/uslu_nicelik/uslu_nicelik.styles.js
@@ -0,0 +1,37 @@
+function __Styles(){
+
+    questionDivStyle = {
+        position:'absolute',
+        top:'30px',
+        left:'80px',
+        width:'400px',
+        height:'80px',
+    //    border:'1px solid',
+        fontSize:'30px'
+    };
+
+    answerDivStyle = {
+        position:'absolute',
+        top:'160px',
+        left:'10px',
+        width:'510px',
+        height:'40px',
+   //     border:'1px solid',
+        color:'#069',
+        fontSize:'22px',
+        textAlign:'center',
+        lineHeight:'24px',
+        opacity:0
+    };
+
+    animDivStyle = {
+        position:'absolute',
+        top:'40px',
+        left:'200px',
+        width:'500px',
+        height:'140px',
+        fontSize:'32px',
+        border:'1px solid'
+    };
+
+}
