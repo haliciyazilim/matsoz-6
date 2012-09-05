d92c36dedd6096413b8cc49743d41bdd8d9180a9
Alperen Kavun <alperen@halici.com.tr>
2012-09-05 16:47:26 +0300

    neccessary styles are added
    Signed-off-by: Alperen Kavun <alperen@halici.com.tr>

--- app/assets/javascripts/animations/yutan_eleman/yutan_eleman.styles.js
+++ app/assets/javascripts/animations/yutan_eleman/yutan_eleman.styles.js
@@ -6,8 +6,28 @@ function __Styles(){
         left:'330px',
         width:'150px',
         height:'80px',
-    //    border:'1px solid',
         fontSize:'32px'
     };
+
+    questionDivStyle = {
+        position:'absolute',
+        top:'60px',
+        left:'160px',
+        width:'200px',
+        height:'60px',
+        fontSize:'32px'
+    };
+
+    answerDivStyle = {
+        position:'absolute',
+        top:'160px',
+        left:'80px',
+        width:'360px',
+        height:'30px',
+        fontSize:'24px',
+        color:'#069',
+        textAlign:'center',
+    //    border:'1px solid'
+    }
 
 };
