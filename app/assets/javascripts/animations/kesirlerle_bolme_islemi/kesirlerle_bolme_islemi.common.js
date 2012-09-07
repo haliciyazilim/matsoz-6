85a8d55315d5d6924a50545cb178e006d1e50a26
Alperen Kavun <alperen@halici.com.tr>
2012-09-07 13:17:00 +0300

    class will be written from start
    Signed-off-by: Alperen Kavun <alperen@halici.com.tr>

--- app/assets/javascripts/animations/kesirlerle_bolme_islemi/kesirlerle_bolme_islemi.common.js
+++ app/assets/javascripts/animations/kesirlerle_bolme_islemi/kesirlerle_bolme_islemi.common.js
@@ -1,60 +1,7 @@
-var Fraction = Class.extend({
-    init:function(opt){
-        if(opt.denominator == 0){
-            throw "divisionByZero";
-        }
-        else{
-            try{
-                switch(opt.type){
-                    case Fraction.SIMPLE:
-                        this.definition = ""+opt.nominator+" bölü "+opt.denominator;
-                        this.nominator = opt.nominator;
-                        this.denominator = opt.denominator;
-                        this.value = this.nominator / this.denominator;
-                        break;
-                    case Fraction.COMPOUND:
-                        this.definition = ""+opt.nominator+" bölü "+opt.denominator;
-                        this.nominator = opt.nominator;
-                        this.denominator = opt.denominator;
-                        this.value = this.nominator / this.denominator;
-                        break;
-                    case Fraction.MIXED:
-                        this.definition = ""+opt.integer+" tam "+opt.nominator+" bölü "+opt.denominator;
-                        this.nominator = opt.nominator;
-                        this.denominator = opt.denominator;
-                        this.integer = opt.integer;
-                        this.value = this.integer + (this.nominator / this.denominator);
-                }
-            }
-            catch(err){
-                return false;
-            }
-            finally{
-                return true;
-            }
-        }
-    },
+var RationalNumber = Class.extend({
 
-    reduceFraction:function(){
-        try{
-            var a = [];
-            a = Util.reduceFractions(this.nominator, this.denominator);
-            this.nominator = a[0];
-            this.denominator = a[1];
-        }
-        catch(err){
-            return false;
-        }
-        finally{
-            return true;
-        }
-    },
-
-    addWith:function(otherFraction){
-
-    }
 });
 
-Fraction.SIMPLE = 0;
-Fraction.COMPOUND = 1;
-Fraction.MIXED = 2;
+RationalNumber.RATIONAL = 0;
+RationalNumber.SIMPLE = 1;
+RationalNumber.COMPOUND = 2;
