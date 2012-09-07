aabf20ab3acedc30ad103c18290b9aa1816474f9
Alperen Kavun <alperen@halici.com.tr>
2012-09-07 16:20:47 +0300

    new class declerations are ready, waiting for implementation
    Signed-off-by: Alperen Kavun <alperen@halici.com.tr>

--- app/assets/javascripts/animations/kesirlerle_bolme_islemi/kesirlerle_bolme_islemi.common.js
+++ app/assets/javascripts/animations/kesirlerle_bolme_islemi/kesirlerle_bolme_islemi.common.js
@@ -1,7 +1,40 @@
 var RationalNumber = Class.extend({
+    init:function(opt){
 
+    },
+    simplification:function(){
+
+    },
+    denomEqualization:function(otherRationalNumber){
+
+    },
+    addition:function(otherRationalNumber){
+
+    },
+    substraction:function(otherRationalNumber){
+
+    },
+    multiplication:function(otherRationalNumber){
+
+    },
+    division:function(otherRationalNumber){
+
+    },
+    conversion:function(){
+
+    },
+    additionInvert:function(){
+
+    },
+    multiplicationInvert:function(){
+
+    },
+    toHTML:function(fontSize){
+
+    }
 });
 
 RationalNumber.RATIONAL = 0;
 RationalNumber.SIMPLE = 1;
 RationalNumber.COMPOUND = 2;
+RationalNumber.COMPLEX = 3;
