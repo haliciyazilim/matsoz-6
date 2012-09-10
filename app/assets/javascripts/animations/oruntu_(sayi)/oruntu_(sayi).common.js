c295575f474898b753d1a2fae679341d952bb4c6
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-09-10 13:51:08 +0300

    completed

--- app/assets/javascripts/animations/oruntu_(sayi)/oruntu_(sayi).common.js
+++ app/assets/javascripts/animations/oruntu_(sayi)/oruntu_(sayi).common.js
@@ -1,26 +1,37 @@
 var NumberWithShape = function(opt){
+    this.animate = Item.prototype.animate;
     this.position = opt.position;
     this.number = opt.number;
     this.size = opt.size;
+    if(opt.isHiddenNumber)
+        this.isHiddenNumber = opt.isHiddenNumber;
+    else
+        this.isHiddenNumber = false;
 }
+
 NumberWithShape.prototype.draw = function(){
     this.removeShape();
     this.cubeArray = [];
     for(var i=0; i < this.number; i++){
-        var position = this.position.add(
-            this.size  * (i % 2),
-            -this.size * Math.floor(i / 2)
-        ).add(
-            this.size * 2 * Math.floor(i/8),
-            this.size * 4 * Math.floor(i/8)
-        )
+        var position = this.position;
+        if(i < 8){
+            position = position.add(
+                this.size  * (i % 2),
+                -this.size * Math.floor(i / 2)
+            );
+        }
+        else{
+            position = position.add(
+                this.size * Math.floor(i / 4),
+                -this.size * Math.floor(i % 4)
+            );
+        }
         this.cubeArray.push(new Path.Cube(
             position,
             this.size,
             new Point(0.4,0.3)
         ).set_style({strokeColor:'#000',fillColor:'#193'}));
     }
-    console.log(""+this.number)
     if(this.number == 1){
         this.width = this.size;
     }
@@ -34,12 +45,24 @@ NumberWithShape.prototype.draw = function(){
         this.height = this.size * Math.ceil(this.number /2 );
     else
         this.height = this.size * 4;
-
-    this.numberText = new PointText(this.position.add(this.width*0.5,this.size*2+8));
+    this.numberText = new PointText(this.position.add(this.width*0.5,this.size+28));
     this.numberText.justification = 'center';
     this.numberText.content = this.number;
     this.numberText.fontSize = 16;
+
+    this.setOpacity(0);
+
 }
+
+NumberWithShape.prototype.setOpacity = function(opacity){
+    if(this.isHiddenNumber === true){
+        for(var i=0;i<this.cubeArray.length;i++)
+            this.cubeArray[i].opacity = opacity;
+
+        this.numberText.opacity = opacity;
+    }
+}
+
 NumberWithShape.prototype.removeShape = function (){
     if(this.cubeArray)
         $(this.cubeArray).each(function(){this.remove()});
@@ -52,25 +75,78 @@ var Pattern = Class.extend({
         this.coefficient = coefficient;
         this.constant = constant;
         this.length = length;
+        this.cubeSize = 25;
     },
     setHiddenNumber:function(hiddenNumber){
         this.hiddenNumber = hiddenNumber;
     },
     draw:function(position){
-        $(this.numbers).each(function(index){
-            var num = new NumberWithShape({
-                position:position.add(120*index,0),
-                number:this,
-                size:25
-            })
-            num.draw();
-        })
+        var nums = [];
+        var self = this;
+        var hiddenNumberPosition;
+        console.log(""+this,this.hiddenNumber);
+        var totalWidth = 0;
+        do{
+            for(var i=0;i < nums.length;i++){
+                nums[i].removeShape();
+            }
+            nums = [];
+            totalWidth = 0;
+            $(this.numbers).each(function(index){
+                var num = new NumberWithShape({
+                    position:position.add(120*index,0),
+                    number:this,
+                    size:self.cubeSize,
+                    isHiddenNumber:(this == self.hiddenNumber)
+                });
+                num.draw();
+                nums.push(num);
+                totalWidth+= (index==0?0:self.cubeSize) + num.width;
+            });
 
+            this.cubeSize--;
+        }
+        while(totalWidth > 570)
+        position = position.add(-totalWidth*0.5,0)
+
+        totalWidth = 0;
+        for(var i=0;i<nums.length;i++){
+            totalWidth += (i>0?this.cubeSize:0);
+            nums[i].position = position.add(totalWidth,0);
+            nums[i].draw();
+            totalWidth += nums[i].width;
+            if(nums[i].number == this.hiddenNumber)
+                hiddenNumberPosition  = nums[i].numberText.position;
+        }
+        this.numberWithShapes = nums;
+        return hiddenNumberPosition;
     },
-    showHiddenNumber:function(){
+    showHiddenNumber:function(duration,delay){
+        if(duration == undefined || isNaN(duration))
+            duration = 0;
+        if(delay == undefined || isNaN(duration))
+            delay = 0;
+        var hiddenNumber;
+        for(var i=0; i<this.numberWithShapes.length; i++)
+            if(this.numberWithShapes[i].isHiddenNumber === true)
+                hiddenNumber = this.numberWithShapes[i];
+        if(duration == 0)
+            hiddenNumber.setOpacity(1);
+        else{
+            hiddenNumber.opacity = 0;
+            hiddenNumber.animate({
+                style:{opacity:1},
+                duration:duration,
+                delay:delay,
+                update:function(){
+                    this.setOpacity(this.opacity);
+                }
+            });
+        }
 
     }
 });
+
 var LinearPattern = Pattern.extend({
     init:function(coefficient,constant,length){
         this._super(coefficient,constant,length);
@@ -79,26 +155,47 @@ var LinearPattern = Pattern.extend({
             numbers.push(coefficient*i+constant);
         }
         this.numbers = numbers;
+    },
+    toString:function(){
+        return (this.coefficient>1?this.coefficient:"") +
+            "n" +
+            (this.constant > 0 ? " + "+this.constant:(this.constant < 0 ? this.constant: ""));
     }
+
 });
+
 var QuadraticPattern = Pattern.extend({
     init:function(coefficient,constant,length){
         this._super(coefficient,constant,length);
+        this.cubeSize = 20;
         var numbers = [];
         for(var i=1; i<=length; i++){
             numbers.push(coefficient*i*i+constant);
         }
         this.numbers = numbers;
+    },
+    toString:function(){
+        return (this.coefficient>1?this.coefficient:"") +
+            "n²" +
+            (this.constant > 0 ? " + "+this.constant:(this.constant < 0 ? this.constant: ""));
     }
 });
+
 var ExponentialPattern = Pattern.extend({
     init:function(coefficient,constant,base,length){
         this._super(coefficient,constant,length);
         this.base = base;
+        this.cubeSize = 20;
+
         var numbers = [];
         for(var i=1; i<=length; i++){
             numbers.push(coefficient*Math.pow(base,i)+constant);
         }
         this.numbers = numbers;
+    },
+    toString:function(){
+        return (this.coefficient>1?this.coefficient:"") +
+            "" + this.base +"<sup>n</sup>" +
+            (this.constant > 0 ? " + "+this.constant:(this.constant < 0 ? this.constant: ""));
     }
 });
