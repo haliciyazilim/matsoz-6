8985610baf5dada9310f5e0d89bc06a41df1c355
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-09-10 13:51:16 +0300

    half working

--- app/assets/javascripts/animations/oruntu_(sayi)/oruntu_(sayi).interaction.js
+++ app/assets/javascripts/animations/oruntu_(sayi)/oruntu_(sayi).interaction.js
@@ -13,16 +13,36 @@ var Interaction = {
             width:$(container).width(),
             height:$(container).height()
         }
+        Interaction.appendButton({
+            bottom:"40px",
+            right:"40px"
+        });
+        Interaction.appendStatus({
 
-
-
-
+        })
+        Interaction.setRandomGenerator(3);
         Interaction.prepareNextQuestion();
     },
 	nextQuestion: function(randomNumber){
-        Interaction.generatePatters(randomNumber);
-        console.log("I'm here");
-        new QuadraticPattern(1,0,4).draw(new Point(20,175));
+        Interaction.flushInputs();
+        /*<[[TEST*/
+//            randomNumber = 0;
+        /*TEST]]>*/
+//        var pattern = new QuadraticPattern(1,0,4);
+        var pattern = Interaction.generatePattern(randomNumber);
+        var inputPosition = pattern.draw(new Point(290,155));
+        Interaction.appendInput({
+            position:'absolute',
+            top:inputPosition.y,
+            left:inputPosition.x,
+            marginLeft:"-18.5px",
+            marginTop:'-25px'
+        });
+        /*<[[TEST*/
+//            Main.setObjective(pattern.toString());
+        /*TEST]]>*/
+        Interaction.pattern = pattern;
+
     },
 		
 	/*
@@ -44,20 +64,31 @@ var Interaction = {
 	onFail : function(){
 		
     },
-    generatePatters: function(randomNumber){
-        Interaction.patternType = randomNumber;
+    generatePattern: function(randomNumber){
         var pattern;
+        var coefficient = Util.randomInteger(1,5);
+        var constant = Util.randomInteger(0,5);
+        var base = 2;
+        var length = 5;
+        var hiddenNumberIndex = Util.randomInteger(0,length-1);
+        /*<[[TEST*/
+//            coefficient = 4;
+//            constant = 5;
+        /*TEST]]>*/
         switch(randomNumber){
             case 0:
-
-                pattern = new Pattern();
+                pattern = new LinearPattern(coefficient,constant,length);
                 break;
-
             case 1:
+                pattern = new QuadraticPattern(coefficient,constant,length);
                 break;
-
             case 2:
+                pattern = new ExponentialPattern(coefficient,constant,base,length);
                 break;
         }
+        console.log(pattern.numbers,pattern.numbers[hiddenNumberIndex]);
+        pattern.setHiddenNumber(pattern.numbers[hiddenNumberIndex]);
+        Interaction.patternType = randomNumber;
+        return pattern;
     }
 }
