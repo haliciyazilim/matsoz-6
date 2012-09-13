67a70afeda83ef81138d9b22412d5a89cf54ecf6
Eren Halici <eren@halici.com.tr>
2012-09-13 18:11:49 +0300

    Autocommit of precompiled assets

---
+++ public/assets/animations/isin/isin.styles.js
@@ -0,0 +1,1 @@
+function __Styles(){lettersStyle={position:"absolute",top:"106px",left:"242px",width:"308px",height:"40px",fontSize:"28px",textAlign:"center",opacity:0},pencilStyle={position:"absolute",top:"-15px",left:"250px",opacity:0}};
