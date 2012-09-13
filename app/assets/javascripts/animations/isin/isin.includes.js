7ed0b42cb8adc2f2f26a30f0e9e102ecf71d5ef6
Alperen Kavun <alperen@halici.com.tr>
2012-09-13 17:02:09 +0300

    new interaction files are added (both interaction and animation parts are finished)
    Signed-off-by: Alperen Kavun <alperen@halici.com.tr>

---
+++ app/assets/javascripts/animations/isin/isin.includes.js
@@ -0,0 +1,4 @@
+//= require ./isin.styles.js
+//= require ./isin.common.js
+//= require ./isin.animation.js
+//= require ./isin.interaction.js 
---
+++ app/assets/javascripts/animations/isin/isin.interaction.js
@@ -0,0 +1,95 @@
+var Interaction = {
+    
+	getFramework:function(){
+        return 'paper';
+    },
+	images:[
+        
+    ],
+    init:function(container){
+        Interaction.container = container;
+        Main.setObjective('Yandaki alanda iki nokta belirleyerek bir ışın oluşturunuz.');
+        Interaction.paper = {
+            width:$(container).width(),
+            height:$(container).height()
+        };
+
+        $(Interaction.container).append('<button id="again" class="repeat_button" style="position:absolute;bottom:20px;right:250px;"></button>');
+
+        Interaction.prepareNextQuestion();
+    },
+	nextQuestion: function(randomNumber){
+        $('#again').css("opacity",0.4);
+        $('#again').get(0).onclick = null;
+
+        Interaction.clickk = 0;
+
+        var tool = new Tool();
+
+        tool.onMouseDown = function(event){
+
+            if(Interaction.clickk == 0){
+                Interaction.firstPoint = event.downPoint;
+                if(Interaction.firstPoint.x > 40 && Interaction.firstPoint.y > 40 && Interaction.firstPoint.x < 550 && Interaction.firstPoint.y < 210){
+                    Interaction.path = new Path();
+                    Interaction.path.strokeColor = "black";
+                    Interaction.path.strokeWidth = 4;
+                    Interaction.path.add(event.downPoint);
+                    Interaction.circle1 = new Path.Circle(Interaction.firstPoint,9);
+                    Interaction.circle1.strokeColor = "black";
+                    Interaction.circle1.fillColor = "black";
+
+                    Interaction.clickk += 1;
+                }
+            }
+            else if(Interaction.clickk == 1){
+                Interaction.secondPoint = event.downPoint;
+                if(Interaction.secondPoint.x > 40 && Interaction.secondPoint.y > 40 && Interaction.secondPoint.x < 550 && Interaction.secondPoint.y < 210){
+                    Interaction.clickk += 1;
+                    Interaction.path.add(event.downPoint);
+
+                    $('#again').css("opacity",1);
+                    $('#again').get(0).onclick = deleteAll;
+
+                    Interaction.line2 = new Path.Line(new Point(Interaction.secondPoint.findPointTo(Interaction.firstPoint,-30)),new Point(Interaction.secondPoint));
+                    Interaction.line2.strokeColor = "black";
+                    Interaction.line2.strokeWidth = 4;
+
+                    Interaction.circle2 = new Path.Circle(Interaction.secondPoint,9);
+                    Interaction.circle2.strokeColor = "black";
+                    Interaction.circle2.fillColor = "black";
+
+                    Interaction.text1 = new PointText(new Point(Interaction.firstPoint.x, Interaction.firstPoint.y+4));
+                    Interaction.text1.justification = 'center';
+                    Interaction.text1.fillColor = 'white';
+                    Interaction.text1.content = 'A';
+                    Interaction.text1.strokeWidth = '1px';
+
+                    Interaction.text2 = new PointText(new Point(Interaction.secondPoint.x, Interaction.secondPoint.y+4));
+                    Interaction.text2.justification = 'center';
+                    Interaction.text2.fillColor = 'white';
+                    Interaction.text2.content = 'B';
+                    Interaction.text2.strokeWidth = '1px';
+                }
+            }
+            else{
+                return false;
+            }
+        }
+    },
+	preCheck : function(){
+
+    },
+	isAnswerCorrect : function(value){
+
+    },
+	onCorrectAnswer : function(){
+		
+    },
+	onWrongAnswer : function(){
+		
+    },
+	onFail : function(){
+		
+    }
+}
