7a9d8d5f993d78f4b903a2f2ddb30b03ca747013
Alperen Kavun <alperen@halici.com.tr>
2012-09-17 14:55:26 +0300

    new interaction files are added
    Signed-off-by: Alperen Kavun <alperen@halici.com.tr>

---
+++ app/assets/javascripts/animations/duzlemde_iki_dogru/duzlemde_iki_dogru.includes.js
@@ -0,0 +1,4 @@
+//= require ./duzlemde_iki_dogru.styles.js
+//= require ./duzlemde_iki_dogru.common.js
+//= require ./duzlemde_iki_dogru.animation.js
+//= require ./duzlemde_iki_dogru.interaction.js 
