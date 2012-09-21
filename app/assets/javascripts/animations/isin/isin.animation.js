3a262e537aed0e74fa8956e127e076e80cfcc431
Kavun <alperen@halici.com.tr>
2012-09-21 13:12:10 +0300

    little bug fixed
    Signed-off-by: Kavun <alperen@halici.com.tr>

--- app/assets/javascripts/animations/isin/isin.animation.js
+++ app/assets/javascripts/animations/isin/isin.animation.js
@@ -9,24 +9,28 @@ var Animation = {
         var dotStart = lineStart+5500;
         var lettersStart = dotStart+2000;
 
-        Animation.dot1 = new Path.Circle(new Point(237,80),6);
+        Animation.dot1 = new Path.Circle(new Point(237,50),6);
         Animation.dot1.strokeColor = "black";
         Animation.dot1.fillColor = "black";
         Animation.dot1.opacity = 0;
 
-        Animation.dot2 = new Path.Circle(new Point(525,80),6);
+        Animation.dot2 = new Path.Circle(new Point(525,50),6);
         Animation.dot2.strokeColor = "black";
         Animation.dot2.fillColor = "black";
         Animation.dot2.opacity = 0;
 
-        Animation.arrow = new Path.OneSidedArrow(new Point(587.5,80.5), new Point(588.5,80.5), 10,30);
+        Animation.arrow = new Path.OneSidedArrow(new Point(587.5,50.5), new Point(588.5,50.5), 10,30);
         Animation.arrow.opacity = 0;
 
         var letters = Util.dom({parent:Animation.container, tag:'div', css:lettersStyle,
             html:'<span style="float:left">A</span> <span style="float:right">B</span><span style="position:relative;top:30px;">[AB</span>&nbsp;&nbsp;&nbsp;<span style="position:relative;top:30px;">AB</span>'
         });
+        var letters2 = Util.dom({parent:Animation.container, tag:'div', css:letters2Style,
+            html:'<span>AB ışını</span>'
+        });
 
-        var myArrow = new Path.OneSidedArrow(new Point(400,116),new Point(440,116),10,20);
+
+        var myArrow = new Path.OneSidedArrow(new Point(400,86),new Point(440,86),10,20);
         myArrow.opacity = 0;
 
         var pencil = Util.dom({parent:Animation.container, tag:'div', css:pencilStyle,
@@ -52,7 +56,7 @@ var Animation = {
                 if(Animation.line){
                     Animation.line.remove();
                 }
-                Animation.line = new Path.Line(new Point(237.5,80.5), new Point(237.5+this.X,80.5));
+                Animation.line = new Path.Line(new Point(237.5,50.5), new Point(237.5+this.X,50.5));
                 Animation.line.strokeColor = "black";
                 Animation.line.strokeWidth = 3;
             },
@@ -89,7 +93,8 @@ var Animation = {
             animationType:'easeInOutQuad'
         });
 
-        $(letters).delay(lettersStart).animate({opacity:1},1000,'easeInOutQuad',function(){Main.animationFinished(1000);})
+        $(letters).delay(lettersStart).animate({opacity:1},1000,'easeInOutQuad');
+        $(letters2).delay(lettersStart).animate({opacity:1},1000,'easeInOutQuad',function(){Main.animationFinished(1000);});
 
         myArrow.animate({
             style:{
