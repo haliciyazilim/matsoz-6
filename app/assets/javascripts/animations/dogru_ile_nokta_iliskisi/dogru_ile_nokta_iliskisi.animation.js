403e91167fdcfda54315486f926d5b308b0b88e4
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-09-25 11:42:05 +0300

    animation is finished

--- app/assets/javascripts/animations/dogru_ile_nokta_iliskisi/dogru_ile_nokta_iliskisi.animation.js
+++ app/assets/javascripts/animations/dogru_ile_nokta_iliskisi/dogru_ile_nokta_iliskisi.animation.js
@@ -1,7 +1,34 @@
 var Animation = {
-    images:[],
+    images:[
+        {
+            id:'ankara_dogrusal',
+            src:'/assets/animations/dogru_ile_nokta_iliskisi/ankara_dogrusal.gif'
+        },
+        {
+            id:'halat_dogrusal',
+            src:'/assets/animations/dogru_ile_nokta_iliskisi/halat_dogrusal.gif'
+        }
+    ],
 	init:function(container){
         Animation.container = container;
+        $(Animation.container).append($('#ankara_dogrusal'));
+        $('#ankara_dogrusal').css({
+            position:"absolute",
+            top:'50%',
+            left:'50%',
+            marginLeft:'-125px',
+            marginTop:'-85px'
+        }).delay(6000).animate({marginLeft:'-325px'},1000,function(){
+            $(Animation.container).append($('#halat_dogrusal'));
+                $('#halat_dogrusal').css({
+                    position:"absolute",
+                    top:'50%',
+                    left:'50%',
+                    marginLeft:'60px',
+                    marginTop:'-85px'
+                })
+                Main.animationFinished(4500);
+        });
 
     }
 }
