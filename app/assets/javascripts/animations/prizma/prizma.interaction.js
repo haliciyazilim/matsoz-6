56aea862c3ac79aabdd1658e66116aec5608c0db
Abdullah Karacabey <abdullah@halici.com.tr>
2012-09-25 16:16:40 +0300

    Yapımı hala devam ediyor

--- app/assets/javascripts/animations/prizma/prizma.interaction.js
+++ app/assets/javascripts/animations/prizma/prizma.interaction.js
@@ -23,6 +23,78 @@ var Interaction = {
             right:"150px"
         })
 
+        $(container).append("<div id='tiklama'>")
+        $("#tiklama").css({
+            position:"absolute",
+            width:"100px",
+            height:"20px",
+            right:"50px",
+            top:"100px"
+
+
+        });
+
+        var tik;
+        var tiklama=0;
+        var tool=new Tool();
+        //tool.distanceThreshold = 100;
+        tool.onMouseDown=onMouseUp;
+
+        var birinciTiklama="";
+
+        function onMouseUp(event) {
+
+
+
+            //alert(event.item.position);
+            if(event.item){
+                if(event.item.class=="nokta"){
+                    tiklama++;
+
+                    if(tiklama==1){
+                        event.item.fillColor="white";
+                        tik = new Path(event.item.position);
+                        birinciTiklama=event.item.myId;
+                        console.log("birinci tıklama "+tiklama+" . "+birinciTiklama);
+                        console.log("ID: "+event.item.myId);
+                        $("#tiklama").html("Birinci Tıklama: "+tiklama+" . "+birinciTiklama);
+
+                    }
+                    else if(tiklama==2){
+                        if(birinciTiklama!=event.item.myId){
+
+
+                            event.item.fillColor="white";
+                            tik.add(event.item.position)
+                            tik.strokeColor = 'red';
+
+                            console.log("ikinci tıklama "+tiklama+" . "+birinciTiklama)
+                            console.log("ok");
+
+                            $("#tiklama").html("İkinci Tıklama: "+tiklama+" . "+birinciTiklama);
+                            tiklama=0;
+                        }
+                        else{
+                            $("#tiklama").html("else İkinci Tıklama: "+tiklama+" . "+birinciTiklama);
+                            tiklama=0;
+                        }
+                    }
+                    else{
+
+                        console.log("sıfırladı. "+tiklama)
+                        $("#tiklama").html("Sıfırlandı: "+tiklama);
+                        tiklama=0;
+
+                    }
+                }
+
+
+
+            }
+        }
+
+
+
         Interaction.prepareNextQuestion();
     },
 	nextQuestion: function(randomNumber){
