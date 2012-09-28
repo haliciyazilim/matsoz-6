8efc84f6ecb3b07d79d31b6d836860e7005d51bc
Eren Halici <eren@halici.com.tr>
2012-09-28 17:09:40 +0300

    Added labels to uzay.animation.js

--- app/assets/javascripts/animations/uzay/uzay.animation.js
+++ app/assets/javascripts/animations/uzay/uzay.animation.js
@@ -3,8 +3,11 @@ var Animation = {
 	init:function(container){
         Animation.container = container;
 
+        var size = 80;
 
-        var matrix = Util.createProjectionMatrix(757, 170, 200, 0, 0, 757, 170);
+
+        var matrix = Util.createProjectionMatrix(757, 170, 200, 0, -20, 757, 170);
+
         var point1 = new Point3(0,0,-200);
 
         var p1 = Util.project(point1, matrix);
@@ -15,7 +18,7 @@ var Animation = {
         var point;
         var line;
         var surface;
-        var cube;
+        var space;
 
         point = new Path.Line(p1, p1.add(2,0));
         point.strokeColor = strokeColor;
@@ -24,19 +27,19 @@ var Animation = {
         var animationHelper = new AnimationHelper({
             lineAngle: -0.05,
             surfaceAngle: 0,
-            cubeAngleX: 0,
-            cubeAngleY: 0
+            spaceAngleX: 0,
+            spaceAngleY: 0
         })
 
 
-        var point2 = new Point3(0,0,-300);
+        var point2 = new Point3(0,0,-200-size*2);
 
         animationHelper.animate({
             style: {
                 lineAngle: -Math.PI/2
             },
             duration: 2000,
-            delay: 2000,
+            delay: 3000,
             animationType: 'easeInEaseOut',
             init: function() {
                 point.remove();
@@ -45,7 +48,7 @@ var Animation = {
                 if (line) {
                     line.remove();
                 }
-                var center = new Point3(-25, 0, -225);
+                var center = new Point3(-size/2, 0, -200 - size/2);
                 var p1 = Util.project(point1.getRotatedPointByY(this.lineAngle, center), matrix);
                 var p2 = Util.project(point2.getRotatedPointByY(this.lineAngle, center), matrix);
 
@@ -54,12 +57,72 @@ var Animation = {
             }
         })
 
+        $(Animation.container).append('<div id="point">Nokta</div>')
+        $('#point').css({
+            position: 'absolute',
+            width: '757px',
+            bottom: '16px',
+            textAlign: 'center',
+            color: 'black',
+            opacity: 0
+        })
+        $(Animation.container).append('<div id="line">Doğru Parçası</div>')
+        $('#line').css({
+            position: 'absolute',
+            width: '757px',
+            bottom: '16px',
+            textAlign: 'center',
+            color: 'black',
+            opacity: 0
+        })
+        $(Animation.container).append('<div id="surface">Düzlem</div>')
+        $('#surface').css({
+            position: 'absolute',
+            width: '757px',
+            bottom: '16px',
+            textAlign: 'center',
+            color: 'black',
+            opacity: 0
+        })
+        $(Animation.container).append('<div id="space">Uzay</div>')
+        $('#space').css({
+            position: 'absolute',
+            width: '757px',
+            bottom: '16px',
+            textAlign: 'center',
+            color: 'black',
+            opacity: 0
+        })
+
+        $('#point').delay(000).animate({
+            opacity: 1
+        }, 1000).delay(2000).animate({
+            opacity: 0
+        }, 1000);
+
+        $('#line').delay(3000).animate({
+            opacity: 1
+        }, 1000).delay(2000).animate({
+            opacity: 0
+        }, 1000);
+
+        $('#surface').delay(6000).animate({
+            opacity: 1
+        }, 1000).delay(2000).animate({
+            opacity: 0
+        }, 1000);
+
+        $('#space').delay(9000).animate({
+            opacity: 1
+        }, 1000);
+
+
         animationHelper.animate({
             style: {
                 surfaceAngle: Math.PI/2
             },
             duration: 2000,
-            delay: 5000,
+            delay: 6000,
             animationType: 'easeInEaseOut',
             init: function() {
                 if (line) {
@@ -71,12 +134,12 @@ var Animation = {
                     surface.remove();
                 }
 
-                var center = new Point3(0, 0, -250);
+                var center = new Point3(0, 0, -200 - size);
 
-                var point1 = new Point3(-50, 0, -200);
-                var point2 = new Point3( 50, 0, -200);
-                var point3 = new Point3( 50, 0, -300);
-                var point4 = new Point3(-50, 0, -300);
+                var point1 = new Point3(-size, 0, -200);
+                var point2 = new Point3( size, 0, -200);
+                var point3 = new Point3( size, 0, -200-size*2);
+                var point4 = new Point3(-size, 0, -200-size*2);
 
                 var p1 = Util.project(point1.getRotatedPointByX(this.surfaceAngle, center), matrix);
                 var p2 = Util.project(point2.getRotatedPointByX(this.surfaceAngle, center), matrix);
@@ -97,41 +160,41 @@ var Animation = {
 
         animationHelper.animate({
             style: {
-                cubeAngleX: Math.PI/8,
-                cubeAngleY: -Math.PI/4
+                spaceAngleX: Math.PI/8,
+                spaceAngleY: -Math.PI/4
             },
             duration: 2000,
-            delay: 8000,
+            delay: 9000,
             animationType: 'easeInEaseOut',
             init: function() {
                 surface.remove();
             },
             update: function() {
-                if (cube) {
-                    cube.remove();
+                if (space) {
+                    space.remove();
                 }
 
-                var center = new Point3(0, 0, -300);
+                var center = new Point3(0, 0, -200-size*2);
 
-                var point1 = new Point3(-50, -50, -250);
-                var point2 = new Point3( 50, -50, -250);
-                var point3 = new Point3( 50,  50, -250);
-                var point4 = new Point3(-50,  50, -250);
+                var point1 = new Point3(-size, -size, -200-size);
+                var point2 = new Point3( size, -size, -200-size);
+                var point3 = new Point3( size,  size, -200-size);
+                var point4 = new Point3(-size,  size, -200-size);
 
-                var point5 = new Point3( 50, -50, -350);
-                var point6 = new Point3( 50,  50, -350);
-                var point7 = new Point3(-50,  50, -350);
+                var point5 = new Point3( size, -size, -200-size*3);
+                var point6 = new Point3( size,  size, -200-size*3);
+                var point7 = new Point3(-size,  size, -200-size*3);
 
-                var p1 = Util.project(point1.getRotatedPointByY(this.cubeAngleY, center).getRotatedPointByX(this.cubeAngleX, center), matrix);
-                var p2 = Util.project(point2.getRotatedPointByY(this.cubeAngleY, center).getRotatedPointByX(this.cubeAngleX, center), matrix);
-                var p3 = Util.project(point3.getRotatedPointByY(this.cubeAngleY, center).getRotatedPointByX(this.cubeAngleX, center), matrix);
-                var p4 = Util.project(point4.getRotatedPointByY(this.cubeAngleY, center).getRotatedPointByX(this.cubeAngleX, center), matrix);
+                var p1 = Util.project(point1.getRotatedPointByY(this.spaceAngleY, center).getRotatedPointByX(this.spaceAngleX, center), matrix);
+                var p2 = Util.project(point2.getRotatedPointByY(this.spaceAngleY, center).getRotatedPointByX(this.spaceAngleX, center), matrix);
+                var p3 = Util.project(point3.getRotatedPointByY(this.spaceAngleY, center).getRotatedPointByX(this.spaceAngleX, center), matrix);
+                var p4 = Util.project(point4.getRotatedPointByY(this.spaceAngleY, center).getRotatedPointByX(this.spaceAngleX, center), matrix);
 
-                var p5 = Util.project(point5.getRotatedPointByY(this.cubeAngleY, center).getRotatedPointByX(this.cubeAngleX, center), matrix);
-                var p6 = Util.project(point6.getRotatedPointByY(this.cubeAngleY, center).getRotatedPointByX(this.cubeAngleX, center), matrix);
-                var p7 = Util.project(point7.getRotatedPointByY(this.cubeAngleY, center).getRotatedPointByX(this.cubeAngleX, center), matrix);
+                var p5 = Util.project(point5.getRotatedPointByY(this.spaceAngleY, center).getRotatedPointByX(this.spaceAngleX, center), matrix);
+                var p6 = Util.project(point6.getRotatedPointByY(this.spaceAngleY, center).getRotatedPointByX(this.spaceAngleX, center), matrix);
+                var p7 = Util.project(point7.getRotatedPointByY(this.spaceAngleY, center).getRotatedPointByX(this.spaceAngleX, center), matrix);
 
-                cube = new Group();
+                space = new Group();
 
                 var s3 = new Path();
                 s3.moveTo(p3);
@@ -140,7 +203,7 @@ var Animation = {
                 s3.lineTo(p4);
                 s3.closed = true;
                 s3.set_style(fillStyle);
-                cube.addChild(s3);
+                space.addChild(s3);
 
                 var s2 = new Path();
                 s2.moveTo(p2);
@@ -149,7 +212,7 @@ var Animation = {
                 s2.lineTo(p3);
                 s2.closed = true;
                 s2.set_style(fillStyle);
-                cube.addChild(s2);
+                space.addChild(s2);
 
                 var s1 = new Path();
                 s1.moveTo(p1);
@@ -158,12 +221,13 @@ var Animation = {
                 s1.lineTo(p4);
                 s1.closed = true;
                 s1.set_style(fillStyle);
-                cube.addChild(s1);
+                space.addChild(s1);
+            },
+            callback: function() {
+                Main.animationFinished();
             }
         })
 
-
-        Main.animationFinished(1000);
     }
 
 }
