d38cc39b069dfa7ec5b7c25d841c00e4fe07cf68
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-10-01 11:15:46 +0300

    interaction is finished

--- app/assets/javascripts/animations/es_cokgenler/es_cokgenler.includes.js
+++ app/assets/javascripts/animations/es_cokgenler/es_cokgenler.includes.js
@@ -1,4 +1,5 @@
+//= require ../benzer_cokgenler/benzer_cokgenler.common.js
 //= require ./es_cokgenler.styles.js
 //= require ./es_cokgenler.common.js
 //= require ./es_cokgenler.animation.js
-//= require ./es_cokgenler.interaction.js 
+//= require ./es_cokgenler.interaction.js
--- app/assets/javascripts/animations/es_cokgenler/es_cokgenler.interaction.js
+++ app/assets/javascripts/animations/es_cokgenler/es_cokgenler.interaction.js
@@ -8,47 +8,69 @@ var Interaction = {
     ],
     init:function(container){
         Interaction.container = container;
-        Main.setObjective('');
+        Main.setObjective('Yandaki kareli zeminde verilen çokgenin eşini diğer kareli zeminde oluşturunuz ve kontrol ediniz.');
         Interaction.paper = {
             width:$(container).width(),
             height:$(container).height()
         }
-
         Interaction.appendButton({
-            bottom:"40px",
+            bottom:"00px",
             right:"40px"
         });
         Interaction.appendStatus({
-            bottom:"50px",
+            bottom:"10px",
             right:"150px"
         })
-        /*
-        *	Initialize your interaction here
-        */
 
+        Interaction.setRandomGenerator(12)
         Interaction.prepareNextQuestion();
     },
 	nextQuestion: function(randomNumber){
+        Interaction.trial++;
+        Main.interactionProject.activeLayer.removeChildren();
+        Interaction.masterShape = InteractiveGrids.CreateShape(randomNumber);
+        Interaction.masterGrid = new InteractiveGrids({
+            position:new Point(10.5,23.5),
+            size:27,
+            style:{
+                strokeColor:'#000'
+            }
+        }).drawShape(Interaction.masterShape);
+        Interaction.slaveGrid = new InteractiveGrids({
+            position:new Point(300.5,23.5),
+            size:27,
+            style:{
+                strokeColor:'#666'
+            }
+        }).drawShape().createTool();
+    },
+
+    preCheck : function(){
+        if(Interaction.slaveGrid.path.closed != true){
+            Interaction.setStatus("Lütfen bir kapalı şekil çiziniz","alert");
+            return false;
+        }
+    },
+    isAnswerCorrect : function(value){
+        Interaction.slaveGrid.path.strokeColor = "green";
+        return InteractiveGrids.AreShapesSame(Interaction.masterGrid.points,Interaction.slaveGrid.points);
+    },
+    onCorrectAnswer : function(){
+        Interaction.pause();
+    },
+    onWrongAnswer : function(){
 
     },
-		
-	/*
-	*	this function is called inside Interaction.__checkAnswer() function
-	*	if this function returns false, check answer operation is cancelled
-	*/
-	preCheck : function(){
+    onFail : function(){
+        Interaction.setStatus("Yanlış cevap.",false);
+        Interaction.slaveGrid.path.strokeColor = "red";
+        Interaction.pause();
+        AnimationManager.delay(function(){
+            Interaction.slaveGrid.removeShape();
+            Interaction.slaveGrid.drawShape(Interaction.masterShape);
+            Interaction.slaveGrid.path.strokeColor = "green";
+            InteractiveGrids.AreShapesSame(Interaction.masterGrid.points,Interaction.slaveGrid.points)
+        },2000);
 
-    },
-	isAnswerCorrect : function(value){
-
-    },
-	onCorrectAnswer : function(){
-		
-    },
-	onWrongAnswer : function(){
-		
-    },
-	onFail : function(){
-		
     }
 }
