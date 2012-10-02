9aa0e4739d93524dfcd625ad1693369501e6d77f
Abdullah Karacabey <abdullah@halici.com.tr>
2012-10-02 14:50:42 +0300

    Uygulama kısmı bitti.

--- app/assets/javascripts/animations/oranti/oranti.common.js
+++ app/assets/javascripts/animations/oranti/oranti.common.js
@@ -1,15 +1,33 @@
-var sorular={
-    soru1:["2 dakikada 1 sayfa okuyan Metin 120 sayfalık bir kitabı kaç dakikada okur?",2,1,120,x],
-    soru2:["Bir matbaada 10 dakikada 40 kitap basıldığına göre 1 saatte kaç kitap basılır?",10,40,60,x],
-    soru3:["Kilosu 2 lira olan domatesten 10 liraya kaç kilo domates alınır?",1,2,x,10],
-    soru4:["Günde 20 ton eşya gönderen kargo firması ayda kaç ton eşya gönderir?",1,20,30,x],
-    soru5:["3 dakikada 1 çiçek diken bahçıvan 12 çiçeği kaç dakikada diker?",3,1,x,12],
-    soru6:["3 dakikada 1 çiçek diken bahçıvan 36 dakikada kaç çiçek diker?",3,1,36,x],
-    soru7:["60 km’lik yolu 1 saatte giden bir otomobil 120 km lik yolu kaç saatte gider?",1,60,x,120],
-    soru8:["120 km’lik yolu 2 saatte giden bir otomobil 60 km lik yolu kaç saatte gider?",2,120,x,60],
-    soru9:["60 km’lik yolu 1 saatte giden bir otomobil 2 saatte kaç km gider?",1,60,2,x],
-    soru9:["120 km’lik yolu 2 saatte giden bir otomobil 1 saatte kaç km gider?",2,120,1,x]
+/*var sorular={
+    // [soru,ilk bağımsız birim,ilk bağımlı birim, ikinci bağımsız brim, ikinci bağımlı birim]
+    soru1:["2 dakikada 1 sayfa okuyan Metin 120 sayfalık bir kitabı kaç dakikada okur?",2,1,0,120],
+    soru2:["Bir matbaada 10 dakikada 40 kitap basıldığına göre 1 saatte kaç kitap basılır?",10,40,60,0],
+    soru3:["Kilosu 2 lira olan domatesten 10 liraya kaç kilo domates alınır?",1,2,0,10],
+    soru4:["Günde 20 ton eşya gönderen kargo firması ayda kaç ton eşya gönderir?",1,20,30,0],
+    soru5:["3 dakikada 1 çiçek diken bahçıvan 12 çiçeği kaç dakikada diker?",3,1,0,12],
+    soru6:["3 dakikada 1 çiçek diken bahçıvan 36 dakikada kaç çiçek diker?",3,1,36,0],
+    soru7:["60 km’lik yolu 1 saatte giden bir otomobil 120 km lik yolu kaç saatte gider?",1,60,0,120],
+    soru8:["120 km’lik yolu 2 saatte giden bir otomobil 60 km lik yolu kaç saatte gider?",2,120,0,60],
+    soru9:["60 km’lik yolu 1 saatte giden bir otomobil 2 saatte kaç km gider?",1,60,2,0],
+    soru10:["120 km’lik yolu 2 saatte giden bir otomobil 1 saatte kaç km gider?",2,120,1,0]
 
 
 
-}
+}*/
+// [soru,ilk bağımsız birim,ilk bağımlı birim, ikinci bağımsız brim, ikinci bağımlı birim,sonuc birimi,donüştürelecek birim, dönüşecek birim, dönüştürme değeri, dönüşecek değerin dizi id'si]
+sorular=[
+    ["ilkBagimsiz dakikada ilkBagimli sayfa okuyan Metin ikinciBagimli sayfalık bir kitabı kaç dakikada okur?","dakika",2,1,0,120],
+    ["Bir matbaada ilkBagimsiz dakikada ilkBagimli kitap basıldığına göre ikinciBagimsiz saatte kaç kitap basılır?","kitap",10,40,1,0,"saat","dakika",60,3],
+    ["ilkBagimsiz kilosu ilkBagimli lira olan domatesten ikinciBagimli liraya kaç kilo domates alınır?","kilo",1,2,0,10],
+    ["ilkBagimsiz günde ilkBagimli ton eşya gönderen kargo firması ikinciBagimsiz ayda kaç ton eşya gönderir?","ton",1,20,1,0,"ay","gün",30,3],
+    ["ilkBagimsiz dakikada ilkBagimli çiçek diken bahçıvan ikinciBagimli çiçeği kaç dakikada diker?","dakika",3,1,0,12],
+    ["ilkBagimsiz dakikada ilkBagimli çiçek diken bahçıvan ikinciBagimsiz dakikada kaç çiçek diker?","çiçek",3,1,36,0],
+    ["ilkBagimsiz km'lik yolu ilkBagimli saatte giden bir otomobil ikinciBagimsiz km'lik yolu kaç saatte gider?","saat",60,1,120,0],
+    ["ilkBagimsiz km'lik yolu ilkBagimli saatte giden bir otomobil ikinciBagimsiz km'lik yolu kaç saatte gider?","saat",120,2,60,0],
+    ["ilkBagimsiz km'lik yolu ilkBagimli saatte giden bir otomobil ikinciBagimli saatte kaç km gider?","km",60,1,0,2],
+    ["ilkBagimsiz km'lik yolu ilkBagimli saatte giden bir otomobil ikinciBagimli saatte kaç km gider?","km",120,2,0,1]
+
+];
+
+
+
--- app/assets/javascripts/animations/oranti/oranti.interaction.js
+++ app/assets/javascripts/animations/oranti/oranti.interaction.js
@@ -1,34 +1,80 @@
-var Interaction = {
-    
-	getFramework:function(){
+
+Interaction = {
+
+    getFramework:function () {
         return 'paper';
     },
-	images:[
-        
+    images:[
+
     ],
-    init:function(container){
+    init:function (container) {
         Interaction.container = container;
         Main.setObjective('');
         Interaction.paper = {
             width:$(container).width(),
             height:$(container).height()
+
         }
 
+
+
+        $(container).append("<div id='soru'>");
+        $("#soru").css({
+            position:"absolute",
+            width:"100%",
+            top:"35px",
+            textAlign:"center",
+            fontSize:"20px",
+            opacity:1
+        });
 
         Interaction.appendInput({
             position:"absolute",
             width:"70px",
             height:"50px",
-            right:"80px",
-            top:"120px",
+            right:"0",
+            top:"100px",
+            left:"0",
+            margin:"auto",
             textAlign:"center",
             fontSize:"20px",
             opacity:1
+        }, true, false);
+        Interaction.input.id = "girdi";
+        $("#girdi").attr("maxLength", "5");
 
-        },true, true);
-        Interaction.input.id="girdi";
+        $(container).append("<div id='dogruCevap'><div id='kesirD'></div><div id='sonucD'></div></div>");
+        $("#dogruCevap").css({
+            position:"absolute",
+            width:"300px",
+            height:"50px",
 
-        $("#girdi").attr("maxLength","5");
+            left:"165px",
+            top:"170px",
+
+            textAlign:"center",
+            fontSize:"20px",
+            opacity:0,
+            color:"green",
+
+
+
+        });
+        $("#kesirD").css({
+            width:"110px",
+            height:"100%",
+            float:"left",
+            opacity:1
+
+
+        });
+        $("#kesirD").append("<div id='payD' class='kesir'></div><div id='paydaD' class='kesir'></div>");
+        $(".kesir").css({width:"100%",height:"50%"})
+        $("#payD").css({lineHeight:"30px"})
+        $("#paydaD").css({borderTop:"green 2px solid",lineHeight:"30px"});
+
+        $("#sonucD").css({marginTop:"16px",textAlign:"left",opacity:1});
+
 
         Interaction.appendButton({
             bottom:"40px",
@@ -37,32 +83,127 @@ var Interaction = {
         Interaction.appendStatus({
             bottom:"50px",
             right:"150px"
-        })
+        });
+
+        soruSayisi=10;
+
+        Interaction.soruIdArray = Util.getShuffledArray(sorular.length);
+        Interaction.sira = 0;
 
 
         Interaction.prepareNextQuestion();
     },
-	nextQuestion: function(randomNumber){
+    nextQuestion:function (randomNumber) {
+        $("#dogruCevap").animate({opacity:"0"},1000);
+        $("input").css("color","black");
+
+        var simdikiSoruId = Interaction.soruIdArray[Interaction.sira];
+        Interaction.simdikiSoru=sorular[simdikiSoruId];
+        Interaction.soruMetni = Interaction.simdikiSoru[0];
+
+        Interaction.ilkBagimsiz=Interaction.simdikiSoru[2]*Math.floor(Math.random()*4+1);
+        Interaction.ilkBagimli=Interaction.simdikiSoru[3]*Math.floor(Math.random()*4+1);
+        Interaction.ikinciBagimsiz=Interaction.simdikiSoru[4]*Math.floor(Math.random()*4+1);
+        Interaction.ikinciBagimli=Interaction.simdikiSoru[5]*Math.floor(Math.random()*4+1);
+
+
+        Interaction.soruMetni=Interaction.soruMetni.replace("ilkBagimsiz",Interaction.ilkBagimsiz);
+        Interaction.soruMetni=Interaction.soruMetni.replace("ilkBagimli",Interaction.ilkBagimli);
+        Interaction.soruMetni=Interaction.soruMetni.replace("ikinciBagimsiz",Interaction.ikinciBagimsiz);
+        Interaction.soruMetni=Interaction.soruMetni.replace("ikinciBagimli",Interaction.ikinciBagimli);
+
+        $("#soru").html(Interaction.soruMetni);
+
+        Interaction.sira++;
+        if(Interaction.sira==Interaction.sorular.length-1)
+            Interaction.sira=0;
+
+
 
     },
-		
-	/*
-	*	this function is called inside Interaction.__checkAnswer() function
-	*	if this function returns false, check answer operation is cancelled
-	*/
-	preCheck : function(){
+
+    /*
+     *	this function is called inside Interaction.__checkAnswer() function
+     *	if this function returns false, check answer operation is cancelled
+     */
+    preCheck:function () {
 
     },
-	isAnswerCorrect : function(value){
+    isAnswerCorrect:function (value) {
+        if(Interaction.simdikiSoru.length>6){
+            var donusturmeDegeri=Interaction.simdikiSoru[8];
+            var donusturulecekDegerId=Interaction.simdikiSoru[9]
+
+            switch (donusturulecekDegerId){
+                case 1:
+                    Interaction.ilkBagimsiz=Interaction.ilkBagimsiz*donusturmeDegeri;
+                    break;
+                case 2:
+                    Interaction.ilkBagimli=Interaction.ilkBagimli*donusturmeDegeri;
+                    break;
+                case 3:
+                    Interaction.ikinciBagimsiz=Interaction.ikinciBagimsiz*donusturmeDegeri;
+                    break;
+                case 4:
+                    Interaction.ikinciBagimli=Interaction.ikinciBagimli*donusturmeDegeri;
+                    break;
+            }
+        }
+
+
+        if(Interaction.ilkBagimsiz==0)
+            Interaction.sonuc=Interaction.ikinciBagimsiz*Interaction.ilkBagimli/Interaction.ikinciBagimli;
+        else if(Interaction.ilkBagimli==0)
+            Interaction.sonuc=Interaction.ikinciBagimli*Interaction.ilkBagimsiz/Interaction.ikinciBagimsiz;
+        else if(Interaction.ikinciBagimsiz==0)
+            Interaction.sonuc=Interaction.ikinciBagimli*Interaction.ilkBagimsiz/Interaction.ilkBagimli;
+        else if(Interaction.ikinciBagimli==0)
+            Interaction.sonuc=Interaction.ikinciBagimsiz*Interaction.ilkBagimli/Interaction.ilkBagimsiz;
+
+        var sorgu=Interaction.sonuc.toString().indexOf(".");
+        if(sorgu>0)
+            Interaction.sonuc=Interaction.sonuc.toFixed(3)
+
+        if(Interaction.sonuc==value)
+            return true;
 
     },
-	onCorrectAnswer : function(){
-		
+    onCorrectAnswer:function () {
+
     },
-	onWrongAnswer : function(){
-		
+    onWrongAnswer:function () {
+
     },
-	onFail : function(){
-		
+    onFail:function () {
+        Interaction.setStatus('Cevabın yanlış; doğrusu yukarıdadır.',false);
+        Interaction.sonucBirimi=Interaction.simdikiSoru[1];
+
+        if(Interaction.ilkBagimsiz==0){
+            $("#payD").html(Interaction.ilkBagimli+" x "+Interaction.ikinciBagimsiz);
+            $("#paydaD").html(Interaction.ikinciBagimli);
+
+        }
+
+        else if(Interaction.ilkBagimli==0){
+            $("#payD").html(Interaction.ikinciBagimli+" x "+Interaction.ilkBagimsiz);
+            $("#paydaD").html(Interaction.ikinciBagimsiz);
+        }
+
+        else if(Interaction.ikinciBagimsiz==0){
+            $("#payD").html(Interaction.ilkBagimsiz+" x "+Interaction.ikinciBagimli);
+            $("#paydaD").html(Interaction.ilkBagimli);
+        }
+
+        else if(Interaction.ikinciBagimli==0){
+            $("#payD").html(Interaction.ilkBagimli+" x "+Interaction.ikinciBagimsiz);
+            $("#paydaD").html(Interaction.ilkBagimsiz);
+        }
+
+        $("#sonucD").html("&nbsp; = "+Interaction.sonuc.toString().replace(".",",")+" "+Interaction.sonucBirimi);
+
+        $("#dogruCevap").animate({opacity:"1"},1000);
+        $("input").css("color","red");
+
     }
-}
+};
+
