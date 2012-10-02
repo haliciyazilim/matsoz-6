aaa6e3e36a69524341ddaf7fa43f7cab40e77a5c
Eren Halici <eren@halici.com.tr>
2012-10-02 17:13:52 +0300

    Autocommit of precompiled assets

---
+++ public/assets/animations/uzunluklari_olcme/uzunluklari_olcme.styles-f240382fc26368ddf28a284d459735f4.js
@@ -0,0 +1,1 @@
+function __Styles(){animStrokeColors="#000000",meterColor="#ff0000",decimeterColor="#ff6666",centimeterColor="#ff9999",millimeterColor="#ffcccc",decameterColor="#990000",hectometerColor="#66000",kilometerColor="#33000"};
