9cd75103683e438bb863244afe7e596c60bedaa0
Kavun <alperen@halici.com.tr>
2012-10-03 17:32:02 +0300

    Entry kesirle_toplama_islemi is created

---
+++ app/assets/javascripts/animations/kesirle_toplama_islemi/kesirle_toplama_islemi.includes.js
@@ -0,0 +1,4 @@
+//= require ./kesirle_toplama_islemi.styles.js
+//= require ./kesirle_toplama_islemi.common.js
+//= require ./kesirle_toplama_islemi.animation.js
+//= require ./kesirle_toplama_islemi.interaction.js 
