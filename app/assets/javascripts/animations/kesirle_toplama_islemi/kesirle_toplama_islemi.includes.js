a5f87105e95413172783777cc48354c6a759771b
Kavun <alperen@halici.com.tr>
2012-10-03 17:33:08 +0300

    wrong-named-files are removed
    Signed-off-by: Kavun <alperen@halici.com.tr>

--- app/assets/javascripts/animations/kesirle_toplama_islemi/kesirle_toplama_islemi.includes.js
+++
@@ -1,4 +0,0 @@
-//= require ./kesirle_toplama_islemi.styles.js
-//= require ./kesirle_toplama_islemi.common.js
-//= require ./kesirle_toplama_islemi.animation.js
-//= require ./kesirle_toplama_islemi.interaction.js 
