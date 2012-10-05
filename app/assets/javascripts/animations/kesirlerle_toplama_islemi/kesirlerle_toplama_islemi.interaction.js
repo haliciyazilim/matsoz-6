c0acaaedcfc40244964dc2c5b9c18019dff5b729
Kavun <alperen@halici.com.tr>
2012-10-05 17:21:35 +0300

    interaction part will be continued
    Signed-off-by: Kavun <alperen@halici.com.tr>

--- app/assets/javascripts/animations/kesirlerle_toplama_islemi/kesirlerle_toplama_islemi.interaction.js
+++ app/assets/javascripts/animations/kesirlerle_toplama_islemi/kesirlerle_toplama_islemi.interaction.js
@@ -8,34 +8,77 @@ var Interaction = {
     ],
     init:function(container){
         Interaction.container = container;
-        Main.setObjective('');
+        Main.setObjective('Yanda verilen kesirlerin toplamlarını bulunuz ve kontrol ediniz.');
         Interaction.paper = {
             width:$(container).width(),
             height:$(container).height()
-        }
+        };
 
-        Interaction.appendButton({
-            bottom:"40px",
-            right:"40px"
-        });
         Interaction.appendStatus({
-            bottom:"50px",
-            right:"150px"
-        })
-        /*
-        *	Initialize your interaction here
-        */
+            bottom:'30px',
+            right:'150px',
+            width:'340px',
+            height:'26px',
+            textAlign:'center'
+        });
+        Interaction.appendButton({
+            bottom:'30px',
+            right:'30px'
+        });
+
+        Interaction.appendInput({
+            position:'absolute',
+            top:'-8px',
+            left:'202px',
+            height:'32px',
+            width:'42px',
+            fontSize:'22px'
+        });
+
+        Interaction.appendInput({
+            position:'absolute',
+            top:'35px',
+            left:'202px',
+            height:'32px',
+            width:'42px',
+            fontSize:'22px'
+        });
+
+        Interaction.questionDiv = Util.dom({parent:Interaction.container, tag:'div', css:questionDivStyle,
+            html:'<div id="firstFracDiv" style="position:absolute;top:0px;left:0px;width:50px;height:60px;"></div>' +
+                '<span id="intPlus" style="position:absolute;top:20px;left:75px;">+</span>' +
+                '</div><div id="secondFracDiv" style="position:absolute;top:0px;left:96px;width:50px;height:60px;"></div>' +
+                '<span id="intEq" style="position:absolute;top:20px;left:170px;">=</span>' +
+                '<div id="answerLine" style="position:absolute;top:30px;left:200px;width:48px;height:1px;border-top:1px solid;padding:0"></div>'
+        });
+
+        $(Interaction.inputs[0]).attr("max-length",3);
+        $(Interaction.inputs[1]).attr("max-length",3);
+
+        $(Interaction.questionDiv).append(Interaction.inputs[0]);
+        $(Interaction.questionDiv).append(Interaction.inputs[1]);
+
 
         Interaction.prepareNextQuestion();
     },
 	nextQuestion: function(randomNumber){
 
+        $('#firstFracDiv').html('');
+        $('#secondFracDiv').html('');
+
+        Interaction.firstFrac = new RationalNumber({factor:1,nominator:1,denominator:3});
+        Interaction.secondFrac = new RationalNumber({factor:1,nominator:1,denominator:2});
+
+        Interaction.firstH = Interaction.firstFrac.toHTML(24);
+        $(Interaction.firstH).css("border","1px solid");
+        Interaction.secondH = Interaction.secondFrac.toHTML(24);
+        $(Interaction.secondH).css("border","1px solid");
+
+        $('#firstFracDiv').append(Interaction.firstH);
+        $('#secondFracDiv').append(Interaction.secondH);
+
+
     },
-		
-	/*
-	*	this function is called inside Interaction.__checkAnswer() function
-	*	if this function returns false, check answer operation is cancelled
-	*/
 	preCheck : function(){
 
     },
--- app/assets/javascripts/animations/kesirlerle_toplama_islemi/kesirlerle_toplama_islemi.styles.js
+++ app/assets/javascripts/animations/kesirlerle_toplama_islemi/kesirlerle_toplama_islemi.styles.js
@@ -1,3 +1,12 @@
 function __Styles(){
 	rectFillColor = "#ffdead";
+
+    questionDivStyle = {
+        position:'absolute',
+        top:'50px',
+        left:'120px',
+        width:'300px',
+        height:'60px',
+        fontSize:'24px',
+    }
 }
