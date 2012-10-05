b075863acdaffa3fccbd7ed959c921d12db224e7
Yunus Eren Guzelll <yeguzel@halici.com.tr>
2012-10-05 15:30:01 +0300

    style changed

--- app/assets/javascripts/animations/oteleme/oteleme.styles.js
+++ app/assets/javascripts/animations/oteleme/oteleme.styles.js
@@ -3,8 +3,8 @@ function __Styles(){
 	* write your styles here without using 'var' definer
 	*/
     interactionPathWrongStyle = {
-        strokeColor:new RgbColor(1,0,0,0.8),
-        fillColor:new RgbColor(1,0.3,0.3,0.8)
+        strokeColor:new RgbColor(1,0.3,0.3,0.7),
+        fillColor:new RgbColor(1,0.5,0.5,0.7)
     }
     interactionPathCorrectStyle ={
         strokeColor:new RgbColor(0,0.7,0,0.8),
