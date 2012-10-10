018690070d9bbca78ff428058c91b59ebeb39146
Kavun <alperen@halici.com.tr>
2012-10-10 11:48:23 +0300

    interaction part is finished
    Signed-off-by: Kavun <alperen@halici.com.tr>

--- app/assets/javascripts/animations/kesirlerle_carpma_islemi/kesirlerle_carpma_islemi.interaction.js
+++ app/assets/javascripts/animations/kesirlerle_carpma_islemi/kesirlerle_carpma_islemi.interaction.js
@@ -27,28 +27,64 @@ var Interaction = {
             right:'30px'
         });
 
-        Interaction.questionRectangle = new Path.Rectangle(new Point(40,20), new Size(190,140));
+        Interaction.questionRectangle = new Path.Rectangle(new Point(40,20), new Size(120,90));
         Interaction.questionRectangle.strokeColor = interactionRectStrokeColor;
         Interaction.questionRectangle.strokeWidth = 2;
         Interaction.questionRectangle.fillColor = interactionRectFillColor;
 
         var firstFrac = Util.dom({parent:Interaction.container, tag:'div', css:firstFracStyle,
             html:'<div id="fracc1" style="position:absolute;top:0px;left:0px;width:65px;height:60px;"></div>' +
-                '<span id="unit1" style="position:absolute;top:16px;left:70px;">cm</span>'
+                '<span id="unit1" style="position:absolute;top:14px;left:72px;">cm</span>'
         });
 
         var secondFrac = Util.dom({parent:Interaction.container, tag:'div', css:secondFracStyle,
             html:'<div id="fracc2" style="position:absolute;top:0px;left:0px;width:65px;height:60px;"></div>' +
-                '<span id="unit2" style="position:absolute;top:16px;left:70px;">cm</span>'
+                '<span id="unit2" style="position:absolute;top:14px;left:72px;">cm</span>'
         });
 
+        Interaction.appendInput({
+            position:'absolute',
+            top:'30px',
+            left:'432px',
+            height:'32px',
+            width:'42px',
+            fontSize:'22px'
+        });
 
+        Interaction.appendInput({
+            position:'absolute',
+            top:'76px',
+            left:'432px',
+            height:'32px',
+            width:'42px',
+            fontSize:'22px'
+        });
+
+        $(Interaction.container).append('<div id="ansLine" style="position:absolute;top:69px;left:430px;height:1px;width:48px;border-top:2px solid;padding:0;margin:0;"></div>' +
+            '<span id="answerUn" style="position:absolute;top:60px;left:490px;font-size:20px;">cm²</span>');
+
+        $(Interaction.inputs[0]).attr("max-length",3);
+        $(Interaction.inputs[1]).attr("max-length",3);
+
+        $(Interaction.container).append(Interaction.inputs[0]);
+        $(Interaction.container).append(Interaction.inputs[1]);
 
         Interaction.prepareNextQuestion();
     },
 	nextQuestion: function(randomNumber){
-        var digit1 = Util.randomDigit(0,5);
-        var digit2 = Util.randomDigit(0,5);
+
+        if(Interaction.answerDiv){
+            $(Interaction.answerDiv).remove();
+        }
+
+        $('#fracc1').html('');
+        $('#fracc2').html('');
+
+        $(Interaction.inputs[0]).css("color","black");
+        $(Interaction.inputs[1]).css("color","black");
+
+        var digit1 = Util.randomInteger(0,5);
+        var digit2 = Util.randomInteger(0,5);
         var nom1, nom2, denom1, denom2,factor1,factor2;
 
         if(digit1 == 0){
@@ -90,24 +126,29 @@ var Interaction = {
         Interaction.answer = Interaction.firstFrac.multiplication(Interaction.secondFrac);
         Interaction.answer.convertToCompoundForm();
 
-        Interaction.firstH = Interaction.firstFrac.toHTML(24);
+        Interaction.firstH = Interaction.firstFrac.toHTML(20);
         $(Interaction.firstH).css("right","0px");
-        Interaction.secondH = Interaction.secondFrac.toHTML(24);
+        Interaction.secondH = Interaction.secondFrac.toHTML(20);
         $(Interaction.secondH).css("right","0px");
 
         $('#fracc1').append(Interaction.firstH);
         $('#fracc2').append(Interaction.secondH);
     },
-		
-	/*
-	*	this function is called inside Interaction.__checkAnswer() function
-	*	if this function returns false, check answer operation is cancelled
-	*/
 	preCheck : function(){
 
     },
 	isAnswerCorrect : function(value){
-
+        if(value[0] != 0 && value[1] != 0){
+            if(value[0]*Interaction.answer.denominator == value[1]*Interaction.answer.nominator){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
     },
 	onCorrectAnswer : function(){
 		
@@ -116,6 +157,82 @@ var Interaction = {
 		
     },
 	onFail : function(){
-		
+        Interaction.setStatus("Yanlış cevap, doğrusu yukarıda gösterilmiştir!", false);
+        $(Interaction.inputs[0]).css("color","red");
+        $(Interaction.inputs[1]).css("color","red");
+
+        Interaction.showAnswer();
+    },
+    showAnswer : function(){
+        Interaction.answerDiv = Util.dom({parent:Interaction.container, tag:'div', css:answerDivStyle,
+            html:'<div id="sf1" style="position:absolute;top:20px;left:20px;width:40px;height:42px;padding:0;margin:0;line-height:20px;">' +
+                '<div id="sint1" style="width:20px;height:42px;text-align:center;float:left;line-height:42px;"></div>' +
+                '<div id="snom1" style="text-align:center;width:20px;height:20px;float:left;"></div>' +
+                '<div id="sline1" style="height:1px;width:20px;border-top:2px solid;padding:0;float:left;"></div>' +
+                '<div id="sdenom1" style="text-align:center;width:20px;height:20px;float:left;"></div>' +
+                '</div>' +
+                '<span id="t1" style="position:absolute;top:32px;left:66px;">•</span>' +
+                '<div id="sf2" style="position:absolute;top:20px;left:72px;width:40px;height:42px;padding:0;margin:0;line-height:20px;">' +
+                '<div id="sint2" style="width:20px;height:42px;text-align:center;float:left;line-height:42px;"></div>' +
+                '<div id="snom2" style="text-align:center;width:20px;height:20px;float:left;"></div>' +
+                '<div id="sline2" style="height:1px;width:20px;border-top:2px solid;padding:0;float:left;"></div>' +
+                '<div id="sdenom2" style="text-align:center;width:20px;height:20px;float:left;"></div>' +
+                '</div>' +
+                '<span id="ee1" style="position:absolute;top:33px;left:118px;">=</span>' +
+                '<div id="sf3" style="position:absolute;top:20px;left:134px;width:60px;height:42px;padding:0;margin:0;line-height:20px;">' +
+                    '<div id="snom3" style="text-align:center;width:60px;height:20px;float:left;"></div>' +
+                    '<div id="sline3" style="height:1px;width:60px;border-top:2px solid;padding:0;float:left;"></div>' +
+                    '<div id="sdenom3" style="text-align:center;width:60px;height:20px;float:left;"></div>' +
+                '</div>' +
+                '<span id="ee2" style="position:absolute;top:33px;left:200px;">=</span>' +
+                '<div id="ff" style="position:absolute;top:20px;left:216px;width:30px;height:42px;padding:0;margin:0;line-height:20px;">' +
+                '<div id="fnom" style="text-align:center;width:30px;height:20px;float:left;"></div>' +
+                '<div id="fline" style="height:1px;width:30px;border-top:2px solid;padding:0;float:left;"></div>' +
+                '<div id="fdenom" style="text-align:center;width:30px;height:20px;float:left;"></div>' +
+                '</div>'
+        });
+
+        if(Interaction.secondFrac.integer){
+            $('#sf1').css("left","20px");
+            $('#fact1').css("left","38px");
+            $('#t1').css("left","66px");
+        }
+        else{
+            $('#sf1').css("left","30px");
+            $('#fact1').css("left","48px");
+            $('#t1').css("left","76px");
+        }
+
+        var snomStr = "";
+        if(Interaction.firstFrac.integer){
+            $('#sint1').html(Interaction.firstFrac.integer);
+            var intp = Interaction.firstFrac.integer*Interaction.firstFrac.denominator;
+            intp += Interaction.firstFrac.nominator;
+            snomStr += intp+" • ";
+        }
+        else{
+            snomStr += ""+Interaction.firstFrac.nominator+" • "
+        }
+        if(Interaction.secondFrac.integer){
+            $('#sint2').html(Interaction.secondFrac.integer);
+            var intp2 = Interaction.secondFrac.integer*Interaction.secondFrac.denominator;
+            intp2 += Interaction.secondFrac.nominator;
+            snomStr += intp2
+        }
+        else{
+            snomStr += ""+Interaction.secondFrac.nominator;
+
+        }
+        $('#snom1').html(Interaction.firstFrac.nominator);
+        $('#snom2').html(Interaction.secondFrac.nominator);
+
+        $('#sdenom1').html(Interaction.firstFrac.denominator);
+        $('#sdenom2').html(Interaction.secondFrac.denominator);
+
+        $('#snom3').html(snomStr);
+        $('#sdenom3').html(Interaction.answer.denominator);
+
+        $('#fnom').html(Interaction.answer.nominator);
+        $('#fdenom').html(Interaction.answer.denominator);
     }
 }
--- app/assets/javascripts/animations/kesirlerle_carpma_islemi/kesirlerle_carpma_islemi.styles.js
+++ app/assets/javascripts/animations/kesirlerle_carpma_islemi/kesirlerle_carpma_islemi.styles.js
@@ -47,21 +47,32 @@ function __Styles(){
 
     firstFracStyle = {
         position:'absolute',
-        top:'165px',
-        left:'80px',
+        top:'120px',
+        left:'40px',
         width:'100px',
         height:'60px',
-        border:'1px solid',
-        fontSize:'24px'
+//        border:'1px solid',
+        fontSize:'20px'
     };
 
     secondFracStyle = {
         position:'absolute',
-        top:'70px',
-        left:'240px',
+        top:'40px',
+        left:'160px',
         width:'100px',
         height:'60px',
-        border:'1px solid',
-        fontSize:'24px'
+//        border:'1px solid',
+        fontSize:'20px'
+    };
+
+    answerDivStyle = {
+        position:'absolute',
+        top:'150px',
+        left:'145px',
+        width:'400px',
+        height:'80px',
+        fontSize:'18px',
+        //    border:'1px solid',
+        color:'#069'
     };
 }
