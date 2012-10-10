09ceb8eaa9058cc25f9e7069c17035944287e5d5
Kavun <alperen@halici.com.tr>
2012-10-10 13:41:15 +0300

    interaction part is finished
    Signed-off-by: Kavun <alperen@halici.com.tr>

--- app/assets/javascripts/animations/kesirlerle_bolme_islemi/kesirlerle_bolme_islemi.styles.js
+++ app/assets/javascripts/animations/kesirlerle_bolme_islemi/kesirlerle_bolme_islemi.styles.js
@@ -1,5 +1,23 @@
 function __Styles(){
-	/*
-	* write your styles here without using 'var' definer
-	*/
+    rectFillColor = "#ffdead";
+
+    questionDivStyle = {
+        position:'absolute',
+        top:'40px',
+        left:'120px',
+        width:'300px',
+        height:'60px',
+        fontSize:'24px'
+    };
+
+    answerDivStyle = {
+        position:'absolute',
+        top:'130px',
+        left:'172px',
+        width:'400px',
+        height:'80px',
+        fontSize:'18px',
+//        border:'1px solid',
+        color:'#069'
+    };
 }
