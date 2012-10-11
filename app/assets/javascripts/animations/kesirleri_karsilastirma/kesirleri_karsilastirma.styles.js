e18378ac010b7c28a7f30209f0a05838ebd561b0
Alperen Kavun <alperen@halici.com.tr>
2012-10-11 17:06:58 +0300

    interaction part is finished

--- app/assets/javascripts/animations/kesirleri_karsilastirma/kesirleri_karsilastirma.styles.js
+++ app/assets/javascripts/animations/kesirleri_karsilastirma/kesirleri_karsilastirma.styles.js
@@ -1,5 +1,15 @@
 function __Styles(){
-	/*
-	* write your styles here without using 'var' definer
-	*/
+    placeHolderColor = "#bfe8ef";
+    fractionsColor="#e6c181";
+    sortableCursorType="pointer";
+
+    fractionsDivStyle = {
+        position:'absolute',
+        top:'30px',
+        left:'313px',
+        height:'40px',
+        width:'180px',
+//        border:'1px solid',
+        fontSize:'16px'
+    };
 }
