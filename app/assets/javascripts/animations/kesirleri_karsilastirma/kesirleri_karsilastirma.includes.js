cd66fe245ead1d563802613b99ab0b76b6cb959c
Kavun <alperen@halici.com.tr>
2012-10-11 11:00:33 +0300

    Entry kesirleri_karsilastirma is created

---
+++ app/assets/javascripts/animations/kesirleri_karsilastirma/kesirleri_karsilastirma.includes.js
@@ -0,0 +1,4 @@
+//= require ./kesirleri_karsilastirma.styles.js
+//= require ./kesirleri_karsilastirma.common.js
+//= require ./kesirleri_karsilastirma.animation.js
+//= require ./kesirleri_karsilastirma.interaction.js 
