2caac2a87ba2bc87e7e3b0c2921efd1959e1c5b5
Alperen Kavun <alperen@halici.com.tr>
2012-10-15 14:31:47 +0300

    conflict resolved

--- app/assets/javascripts/animations/aci/aci.languages.js
+++ app/assets/javascripts/animations/aci/aci.languages.js
@@ -1,11 +1,3 @@
-/**
- * Created with JetBrains WebStorm.
- * User: yunus_work
- * Date: 13.09.2012
- * Time: 10:08
- * To change this template use File | Settings | File Templates.
- */
-
 function __Languages(){
 
 }
