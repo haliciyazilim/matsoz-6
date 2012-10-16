ec977ad6c82b8a9a909db0ca6ad76334fedb14e2
Alperen Kavun <alperen@halici.com.tr>
2012-10-16 17:42:33 +0300

    animation part will be continued

--- app/assets/javascripts/animations/aci/aci.animation.js
+++ app/assets/javascripts/animations/aci/aci.animation.js
@@ -1,7 +1,13 @@
 var Animation = {
-    images:[],
+    images:[
+        {
+            id:'animPaper',
+            src:'/assets/animations/mavi_cizgili_kagit.jpg'
+        }
+    ],
 	init:function(container){
         Animation.container = container;
-
+        var animPaper = new Raster('animPaper');
+        animPaper.position = new Point(200.5,100.5);
     }
 }
