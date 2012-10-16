d3af38e7885caf960bb555cf14d25e5fd18e7747
Abdullah Karacabey <abdullah@halici.com.tr>
2012-10-16 13:54:46 +0300

    re-removed public assets

--- public/assets/animations/kesirlerle_cikarma_islemi/kesirlerle_cikarma_islemi.styles-54f972eb9a2a20f42450177fbf0c8032.js
+++
@@ -1,1 +0,0 @@
-function __Styles(){rectFillColor="#ffdead",questionDivStyle={position:"absolute",top:"40px",left:"120px",width:"300px",height:"60px",fontSize:"24px"},answerDivStyle={position:"absolute",top:"130px",left:"105px",width:"400px",height:"80px",fontSize:"18px",color:"#069"}};
