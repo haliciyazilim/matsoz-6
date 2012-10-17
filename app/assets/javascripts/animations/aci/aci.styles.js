b847bc11b51995b2b4c111b6669901a6a15de486
Alperen Kavun <alperen@halici.com.tr>
2012-10-17 17:09:07 +0300

    animation part is finished

--- app/assets/javascripts/animations/aci/aci.styles.js
+++ app/assets/javascripts/animations/aci/aci.styles.js
@@ -1,5 +1,7 @@
 function __Styles(){
-	/*
-	* write your styles here without using 'var' definer
-	*/
+	outsideColor = new RgbColor(0.953,0.784,0.518);
+    insideColor = new RgbColor(0.663,0.859,0.898);
+
+    insideColor2 = new RgbColor(0.255,0.506,0.541);
+    outsideColor2 = new RgbColor(0.451,0.467,0.239);
 }
