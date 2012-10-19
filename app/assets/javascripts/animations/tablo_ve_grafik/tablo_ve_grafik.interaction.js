b7b214f2760e7f1a24db4a12795e7400efa9a50f
Alperen Kavun <alperen@halici.com.tr>
2012-10-19 17:04:56 +0300

    animation is added to columnGraph func. interaction and animation parts will be continued

--- app/assets/javascripts/animations/tablo_ve_grafik/tablo_ve_grafik.interaction.js
+++ app/assets/javascripts/animations/tablo_ve_grafik/tablo_ve_grafik.interaction.js
@@ -23,26 +23,65 @@ var Interaction = {
             height:"30px",
 //            border:"1px solid"
         });
-//        var xLabels = ["P.tesi","Sali", "Cuma"];
-//        var yLabels = ["10","8","6","4","2","0"];
-        var xLabels = ["", ""];
-        var yLabels = ["","","","","","0"];
-        var data = [];
+
+        Interaction.appendInput({
+            position:'absolute',
+            top:'20px',
+            left:'400px',
+            width:'150px',
+            height:'30px',
+            fontSize:'12px',
+            color:'#757575'
+        },false,false);
+        $(Interaction.container).append(Interaction.input);
+        Interaction.input.onfocus = function(){
+            if(this.value == 'Tablo basligini giriniz.'){
+                this.value = '';
+                $(Interaction.input).css({
+                    color:'#000000',
+                    fontSize:'12px'
+                });
+            }
+
+        };
+        Interaction.input.onblur = function(){
+            if(this.value == ''){
+                this.value = 'Tablo basligini giriniz.';
+                $(Interaction.input).css({
+                    color:'#757575',
+                    fontSize:'12px'
+                });
+            }
+
+        };
+        $(Interaction.input).attr('maxLength',30);
+
+        var xLabels = ["Pazartesi","Sali", "Carsamba","Persembe"];
+        var yLabels = ["10","8","6","4","2","0"];
+        var data = [9,3,7,8];
+        var xGridLabelStyle = {
+            justification:'right',
+            rotation:-90
+        };
         var chart = {
             xAxisName: "Zaman",
-            xAxisUnit: "Gün",
+//            xAxisUnit: "Gün",
             yAxisName: "Satis fiyati",
-            yAxisUnit: "TL",
+//            yAxisUnit: "TL",
+            xGridLabelStyle: xGridLabelStyle,
             xLabels: xLabels,
             yLabels: yLabels,
             data: data
         };
-        columnGraph(new Point(100,50),chart.xLabels.length*60,120,chart);
+        columnGraph(new Point(40,50),chart.xLabels.length*50,120,chart,undefined,1000,1000);
+
+        Interaction.disableAutoFocus();
 
         Interaction.prepareNextQuestion();
     },
 	nextQuestion: function(randomNumber){
-
+        Interaction.input.focus();
+        Interaction.input.blur();
     },
 	preCheck : function(){
 
