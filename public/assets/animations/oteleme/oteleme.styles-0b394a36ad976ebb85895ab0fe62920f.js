afd07ef911e7d65bffa65be92d350a50dba1a1f1
Eren Halici <eren@halici.com.tr>
2013-01-17 18:14:06 +0200

    Recompiled public assets

--- public/assets/animations/oteleme/oteleme.styles-0b394a36ad976ebb85895ab0fe62920f.js
+++
@@ -1,1 +0,0 @@
-function __Styles(){interactionPathWrongStyle={strokeColor:new RgbColor(1,.3,.3,.7),fillColor:new RgbColor(1,.5,.5,.7)},interactionPathCorrectStyle={strokeColor:new RgbColor(0,.7,0,.8),fillColor:new RgbColor(.3,.7,.3,.8)}};
