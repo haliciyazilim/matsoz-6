82832ae236e58c80d08ce44bbd299e666af89e42
Eren Halici <eren@halici.com.tr>
2013-01-17 18:42:51 +0200

    Added auto-removal of public assets

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,14 @@ git merge development
 sed -i .old 's|//console.log =|console.log =|g' app/assets/javascripts/main.js
 rm app/assets/javascripts/main.js.old
 
-rm -rf public/assets
+if [ -d "public/assets" ]; then
+    # Control will enter here if $DIRECTORY exists.
+	git rm -rf public/assets
+	git commit -m "Auto-deletion of precompiled assets"
+fi
+
 RAILS_ENV=production bundle exec rake assets:precompile
+
 git add public/assets/
 git commit -m "Autocommit of precompiled assets"
 git push heroku master
