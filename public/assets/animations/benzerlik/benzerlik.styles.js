79e06cabeb92b1ee6f68cf2e933c7d371146b71e
Eren Halici <eren@halici.com.tr>
2013-01-21 14:27:49 +0200

    Autocommit of precompiled assets

---
+++ public/assets/animations/benzerlik/benzerlik.styles.js
@@ -0,0 +1,1 @@
+function __Styles(){interactionBoundaryStyle={strokeColor:"#fff"},interactionPathStyle={strokeColor:"#000",strokeWidth:2},interactionCircleStyle={strokeColor:new RgbColor(0,0,0,0),fillColor:new RgbColor(0,0,0,0)}};
---
+++ public/assets/animations/denklem/denklem.styles.js
@@ -0,0 +1,1 @@
+function __Styles(){ilkRenk="#000000",degisenRenk="#ff0000"};
