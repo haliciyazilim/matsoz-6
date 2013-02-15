38b175b95e97b31927a3db5a02dc1335a9255312
Yunus Eren Guzel <exculuber@gmail.com>
2013-02-15 15:48:29 +0200

    bug fix

--- app/assets/javascripts/library/InputReverseWriteable.js
+++ app/assets/javascripts/library/InputReverseWriteable.js
@@ -5,13 +5,27 @@
 function InputReverseWriteable(event) {
     if(this.selectorIndex == undefined)
         this.selectorIndex = 0;
-    console.log(this.selectorIndex);
-    var willSelectorBePlaced = false;
-    console.log('keyCode: ',event.keyCode);
+//    console.log(this.selectorIndex);
+    this.willSelectorBePlaced = false;
+//    console.log('keyCode: ',event.keyCode);
     switch(event.keyCode){
         case 8:
-            willSelectorBePlaced = true;
-            this.selectorIndex = 1;
+            this.willSelectorBePlaced = false;
+            this.selectorIndex = 0;
+            this.value = this.value.substring(1);
+            if(this.createTextRange){// IE
+                var textRange = this.createTextRange();
+                textRange.collapse(true);
+                textRange.moveEnd("character",this.selectorIndex);
+                textRange.moveStart("character",this.selectorIndex);
+                textRange.select();
+                return true;
+            }else if(this.setSelectionRange){
+                this.setSelectionRange(this.selectorIndex,this.selectorIndex);
+                return true;
+            }
+            event.preventDefault();
+            return false;
             break;
         case 37:
             if(this.selectorIndex > 0)
@@ -22,31 +36,26 @@ function InputReverseWriteable(event) {
                 this.selectorIndex++
             break;
         default:
-            willSelectorBePlaced = true;
+            this.willSelectorBePlaced = true;
             if(this.selectorIndex > 0)
                 this.selectorIndex--;
             break;
 
     }
-    if(this.value.indexOf(',') > this.selectorIndex)
-        willSelectorBePlaced = false;
-
-//    console.log(this.selectorIndex)
-    if(willSelectorBePlaced){
-        if(this.createTextRange){// IE
-            var textRange = this.createTextRange();
-            textRange.collapse(true);
-            textRange.moveEnd("character",this.selectorIndex);
-            textRange.moveStart("character",this.selectorIndex);
-            textRange.select();
-            return true;
-        }else if(this.setSelectionRange){
-            this.setSelectionRange(this.selectorIndex,this.selectorIndex);
-            return true;
+    $(this).keyup(function(){
+        if(this.willSelectorBePlaced){
+            if(this.createTextRange){// IE
+                var textRange = this.createTextRange();
+                textRange.collapse(true);
+                textRange.moveEnd("character",this.selectorIndex);
+                textRange.moveStart("character",this.selectorIndex);
+                textRange.select();
+                return true;
+            }else if(this.setSelectionRange){
+                this.setSelectionRange(this.selectorIndex,this.selectorIndex);
+                return true;
+            }
         }
-
-    }
-
-    
+    });
 }
 
