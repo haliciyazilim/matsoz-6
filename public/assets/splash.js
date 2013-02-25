e4af8b30895309908d97df308e224a842297917e
Eren Halici <eren@halici.com.tr>
2013-02-25 16:24:24 +0200

    Auto-deletion of precompiled assets

--- public/assets/splash.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created with JetBrains WebStorm.
- * User: yeguzel
- * Date: 01.11.2012
- * Time: 09:45
- * To change this template use File | Settings | File Templates.
- */
-var Splash=function(){$(document).ready(Splash.init)};Splash.init=function(){Main.initializeNavigation(),$(".navlinktasiyici").css({left:parseInt($(".navlinktasiyici").css("left"),10)-700+"px"}),$(".sozcuktasiyici").css({left:parseInt($(".sozcuktasiyici").css("left"),10)-600+"px"}),$(".btn_home").css({left:parseInt($(".btn_home").css("left"),10)-600+"px"}),$(".btn_info").css({left:parseInt($(".btn_info").css("left"),10)-600+"px"}),$(".btn_print").css({left:parseInt($(".btn_print").css("left"),10)-600+"px"}),setTimeout(function(){Splash.animate()},1e3)},Splash.animate=function(){$(".matsoz_logo").delay(1700).animate({marginLeft:"-150px"},1e3,"easeInOutQuad"),$(".navlinktasiyici").delay(0).animate({left:"12px"},2e3,"easeInOutQuad"),$(".sozcuktasiyici").delay(0).animate({left:"144px"},2e3,"easeInOutQuad"),$(".btn_home").delay(0).animate({left:"12px"},2e3,"easeInOutQuad"),$(".btn_info").delay(0).animate({left:"52px"},2e3,"easeInOutQuad"),$(".btn_print").delay(0).animate({left:"92px"},2e3,"easeInOutQuad")},Splash();
