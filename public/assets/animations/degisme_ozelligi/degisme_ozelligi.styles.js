eda5ec638542947078e72d3b5670d296edd55a12
Eren Halici <eren@halici.com.tr>
2013-03-04 14:06:34 +0200

    Auto-deletion of precompiled assets

--- public/assets/animations/degisme_ozelligi/degisme_ozelligi.styles.js
+++
@@ -1,1 +0,0 @@
-function __Styles(){operatorFillColor="#c00000",operatorTextColor="white",operandBoxesFillColor="#41818a",operandTextColor="white",generalStrokeColor="#101010",disabledInputBackgroundColor="#006e7d",disabledInputOpacity=.3,selectedInputStrokeColor="#006e7d",selectedInputStrokeWidth=2};
