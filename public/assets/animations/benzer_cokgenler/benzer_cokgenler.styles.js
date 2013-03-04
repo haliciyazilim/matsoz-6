8ca7210878030060d24c80c8f34c1ff84774a2de
Eren Halici <eren@halici.com.tr>
2013-03-04 15:13:08 +0200

    Auto-deletion of precompiled assets

--- public/assets/animations/benzer_cokgenler/benzer_cokgenler.styles.js
+++
@@ -1,5 +0,0 @@
-function __Styles(){
-	pantographShapesFillColor = "#f2c885";
-    pantographShapesStrokeColor = "#9b763d";
-}
-;
--- public/assets/animations/en_buyuk_ortak_bolen/en_buyuk_ortak_bolen.styles.js
+++
@@ -1,5 +0,0 @@
-function __Styles(){
-    answerDivColor = "#069";
-    animColor = "#ff0000";
-}
-;
