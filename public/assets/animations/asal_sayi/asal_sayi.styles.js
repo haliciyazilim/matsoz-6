ebd875c72f1bf05fcb64c9bfdad9c0f4e015bce9
Eren Halici <eren@halici.com.tr>
2013-03-04 17:47:00 +0200

    Auto-deletion of precompiled assets

--- public/assets/animations/asal_sayi/asal_sayi.styles.js
+++
@@ -1,1 +0,0 @@
-function __Styles(){wholeTableFillColor="white",wholeTableStrokeColor="#a8dbe3",wholeTablePrimeFillColor="#9ee9a5",wholeTablePrimeStrokeColor="#4f9c4f",counterColor="#a8dbe3",animationRectFillColor="#e99e9e",animationRectStrokeColor="#9c4f4f",animationRectFillColor2="#f2c885",animationRectStrokeColor2="#9b763d"};
