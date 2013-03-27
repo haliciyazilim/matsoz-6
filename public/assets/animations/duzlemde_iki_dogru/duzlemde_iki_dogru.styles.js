718b80d077a17e9f6efc72b38587b0da50c46805
Eren Halici <eren@halici.com.tr>
2013-03-27 17:39:04 +0200

    Autocommit of precompiled assets

---
+++ public/assets/animations/duzlemde_iki_dogru/duzlemde_iki_dogru.styles.js
@@ -0,0 +1,1 @@
+function __Styles(){interactionRectangleStyle={fillColor:"#ffffec"},interactionLineStyle={strokeWidth:4,strokeCap:"round"},interactionLineSelectedStyle={strokeColor:new RgbColor(.12,.34,.63)},interactionLineWrongStyle={strokeColor:new RgbColor(.8,.25,.18)},interactionLineCorrectStyle={strokeColor:new RgbColor(.37,.66,.14)},markLineColor="#d8d8d8"};
